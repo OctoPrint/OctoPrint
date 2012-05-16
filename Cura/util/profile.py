4c8086d29475fb8f2bab618f6e866a3627ff2f56
daid <daid303@gmail.com>
2012-05-16 16:47:51 +0200

    By default turn the fan off in the start code, because we enable it on the 2nd layer.

--- Cura/util/profile.py
+++ Cura/util/profile.py
@@ -80,6 +80,7 @@ alterationDefault = {
 	'start.gcode': """;Start GCode
 G21        ;metric values
 G90        ;absolute positioning
+M107       ;start with the fan off
 
 G28 X0 Y0  ;move X/Y to min endstops
 G28 Z0     ;move Z to min endstops
