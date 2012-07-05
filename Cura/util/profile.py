6e245ed7feefd6435364e7c06ff1bb395681294d
daid <daid303@gmail.com>
2012-07-05 16:08:18 +0200

    Updated start/end code.

--- Cura/util/profile.py
+++ Cura/util/profile.py
@@ -92,28 +92,23 @@ M107       ;start with the fan off
 
 G28 X0 Y0  ;move X/Y to min endstops
 G28 Z0     ;move Z to min endstops
-
-; if your prints start too high, try changing the Z0.0 below
-; to Z1.0 - the number after the Z is the actual, physical
-; height of the nozzle in mm. This can take some messing around
-; with to get just right...
 G92 X0 Y0 Z0 E0         ;reset software position to front/left/z=0.0
-G1 Z15.0 F{max_z_speed} ;move the platform down 15mm
-G92 E0                  ;zero the extruded length
 
-G1 F200 E5              ;extrude 5mm of feed stock
-G1 F200 E3.5            ;reverse feed stock by 1.5mm
+G1 Z15.0 F{max_z_speed} ;move the platform down 15mm
+
+G92 E0                  ;zero the extruded length
+G1 F200 E3              ;extrude 3mm of feed stock
 G92 E0                  ;zero the extruded length again
 
-;go to the middle of the platform, and move to Z=0 before starting the print.
-G1 X{machine_center_x} Y{machine_center_y} F{travel_speed}
-G1 Z0.0 F{max_z_speed}
+G1 X{machine_center_x} Y{machine_center_y} F{travel_speed} ;go to the middle of the platform
 """,
 #######################################################################################
 	'end.gcode': """;End GCode
 M104 S0                    ;extruder heat off
 G91                        ;relative positioning
-G1 Z+10 E-5 F{max_z_speed} ;move Z up a bit and retract filament by 5mm
+G1 E-1 F300                ;retract the filament a bit before lifting the nozzle, to release some of the pressure
+G1 Z+0.5 E-5 F{max_z_speed};move Z up a bit and retract filament even more
+G1 Z+10 F{max_z_speed}     ;move Z up a bit more without retraction
 G28 X0 Y0                  ;move X/Y to min endstops, so the head is out of the way
 M84                        ;steppers off
 G90                        ;absolute positioning
