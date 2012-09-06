8a7837a8ce7b35c3141374c6a5c99361261fa70a
daid <daid303@gmail.com>
2012-09-06 10:10:05 +0200

    Add ATMega1280 chip to programmer chips.

--- Cura/avr_isp/chipDB.py
+++ Cura/avr_isp/chipDB.py
@@ -1,5 +1,10 @@
 
 avrChipDB = {
+	'ATMega1280': {
+		'signature': [0x1E, 0x97, 0x03],
+		'pageSize': 128,
+		'pageCount': 512,
+	},
 	'ATMega2560': {
 		'signature': [0x1E, 0x98, 0x01],
 		'pageSize': 128,
