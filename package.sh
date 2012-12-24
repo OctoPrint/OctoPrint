5739b1147173c404bed97e4acd766d1a58ce79c4
daid303 <daid303@gmail.com>
2012-12-24 10:17:52 +0100

    Add power python module to package script.

--- package.sh
+++ package.sh
@@ -150,6 +150,9 @@ if [ $BUILD_TARGET = "win32" ]; then
 	downloadURL http://www.uwe-sieber.de/files/ejectmedia.zip
 	#Get pypy
 	downloadURL https://bitbucket.org/pypy/pypy/downloads/pypy-${PYPY_VERSION}-win32.zip
+	#Get the power module for python
+	rm -rf Power
+	git clone https://github.com/GreatFruitOmsk/Power
 else
 	downloadURL https://bitbucket.org/pypy/pypy/downloads/pypy-${PYPY_VERSION}-${BUILD_TARGET}.tar.bz2
 fi
@@ -183,10 +186,13 @@ if [ $BUILD_TARGET = "win32" ]; then
 	mv PURELIB/OpenGL ${TARGET_DIR}/python/Lib
 	mv PURELIB/comtypes ${TARGET_DIR}/python/Lib
 	mv PLATLIB/numpy ${TARGET_DIR}/python/Lib
+	mv Power/power ${TARGET_DIR}/python/Lib
 	mv VideoCapture-0.9-5/Python27/DLLs/vidcap.pyd ${TARGET_DIR}/python/DLLs
 	mv ffmpeg-20120927-git-13f0cd6-win32-static/bin/ffmpeg.exe ${TARGET_DIR}/Cura/
 	mv ffmpeg-20120927-git-13f0cd6-win32-static/licenses ${TARGET_DIR}/Cura/ffmpeg-licenses/
 	mv Win32/EjectMedia.exe ${TARGET_DIR}/Cura/
+	
+	rm -rf Power/
 	rm -rf \$_OUTDIR
 	rm -rf PURELIB
 	rm -rf PLATLIB
