766e9f0e7fcd1f90ff48e99b514dd207fbdb39d1
Gina Häußge <osd@foosel.net>
2013-03-02 13:38:30 +0100

    Show custom title first in head in case of small tabs

--- octoprint/static/js/ui.js
+++ octoprint/static/js/ui.js
@@ -208,9 +208,16 @@ function AppearanceViewModel(settingsViewModel) {
     self.name = settingsViewModel.appearance_name;
     self.color = settingsViewModel.appearance_color;
 
-    self.title = ko.computed(function() {
+    self.brand = ko.computed(function() {
         if (self.name())
             return "OctoPrint: " + self.name();
+        else
+            return "OctoPrint";
+    })
+
+    self.title = ko.computed(function() {
+        if (self.name())
+            return self.name() + " [OctoPrint]";
         else
             return "OctoPrint";
     })
