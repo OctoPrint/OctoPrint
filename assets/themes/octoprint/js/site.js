04d8bfe44be9eb9648c1e1d45cd1176fbc83cca9
Gina Häußge <osd@foosel.net>
2013-03-31 18:35:19 +0200

    Some more reshuffling of stuff

--- assets/themes/octoprint/js/site.js
+++ assets/themes/octoprint/js/site.js
@@ -1,31 +1,13 @@
 $(function() {
-  // start the icon carousel
-  $('#iconCarousel').carousel({
+  // start the screenshot carousel
+  $('#screenshot-carousel').carousel({
     interval: 5000
   });
 
   // make code pretty
   window.prettyPrint && prettyPrint();
 
-  // inject twitter & github counts
-  $.ajax({
-    url: 'https://api.github.com/repos/foosel/OctoPrint',
-    dataType: 'jsonp',
-    success: function(data) {
-      $('#watchers').html(data.data.watchers);
-      $('#forks').html(data.data.forks);
-    }
-  });
-
-  // inject flattr counts
-  $.ajax({
-    url: 'https://api.flattr.com/rest/v2/things/1179085',
-    dataType: 'json',
-    success: function(data) {
-      $('#flattrs').html(data.flattrs);
-    }
-  });
-
+  // Flattr
   (function() {
       var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
       s.type = 'text/javascript';
