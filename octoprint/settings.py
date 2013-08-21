3e0abf1b2074b8beb05694d988e7f0d2a47f4b28
Gina Häußge <osd@foosel.net>
2013-08-21 20:33:55 +0200

    New custom control type "feedback"
    Like "feedback_command", but without the button

--- octoprint/settings.py
+++ octoprint/settings.py
@@ -248,7 +248,7 @@ class Settings(object):
 		return feedbackControls
 
 	def _getFeedbackControls(self, control=None):
-		if control["type"] == "feedback_command":
+		if control["type"] == "feedback_command" or control["type"] == "feedback":
 			pattern = control["regex"]
 			try:
 				matcher = re.compile(pattern)
