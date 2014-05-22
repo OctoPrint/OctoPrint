604c34e7149137af1a3ada500060cadae7e10c6e
Salandora <Sunpack@web.de>
2014-05-22 12:44:49 +0200

    Bug fix for Emergency Stop

--- octoprint/util/comm.py
+++ octoprint/util/comm.py
@@ -966,6 +966,10 @@ class MachineCom(object):
 		self._resendDelta = None
 
 		return None
+		
+	def _gcode_M112(self, cmd): # It's an emergency what todo? Canceling the print should be the minimum
+		self.cancelPrint()
+		return cmd
 
 ### MachineCom callback ################################################################################################
 
