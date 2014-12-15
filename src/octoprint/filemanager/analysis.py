2ec31c7521b4c771e7e4568515fca8e9151c92d3
Gina Häußge <osd@foosel.net>
2014-12-15 11:38:17 +0100

    Removed a left-over from print time estimation experiments that got overlooked

--- src/octoprint/filemanager/analysis.py
+++ src/octoprint/filemanager/analysis.py
@@ -150,8 +150,6 @@ class GcodeAnalysisQueue(AbstractAnalysisQueue):
 			result = dict()
 			if self._gcode.totalMoveTimeMinute:
 				result["estimatedPrintTime"] = self._gcode.totalMoveTimeMinute * 60
-			if self._gcode.timeDistribution:
-				result["printTimeDistribution"] = self._gcode.timeDistribution
 			if self._gcode.extrusionAmount:
 				result["filament"] = dict()
 				for i in range(len(self._gcode.extrusionAmount)):
