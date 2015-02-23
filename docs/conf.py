2fac5c76d9c605d669900cdfd4440fbeeda5fb6c
Gina Häußge <osd@foosel.net>
2015-02-23 22:15:04 +0100

    Fixed styling of tables in docs

--- docs/conf.py
+++ docs/conf.py
@@ -139,6 +139,11 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
+    ],
+}
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
