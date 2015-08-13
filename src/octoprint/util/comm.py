5f53b704dddf87b9a2e0eeac26bcc4445898bcd5
Gina Häußge <osd@foosel.net>
2015-08-13 17:22:26 +0200

    Use connection closing flag only for exception handling
    Otherwise we kill the beforePrinterDisconnected gcode script...
    
    See #1021
    (cherry picked from commit 2fe956e)

--- src/octoprint/util/comm.py
+++ src/octoprint/util/comm.py
@@ -1326,7 +1326,7 @@ class MachineCom(object):
 		return line
 
 	def _readline(self):
-		if self._serial == None or self._connection_closing:
+		if self._serial is None:
 			return None
 
 		try:
@@ -1646,7 +1646,7 @@ class MachineCom(object):
 		self._doSendWithoutChecksum(commandToSend)
 
 	def _doSendWithoutChecksum(self, cmd):
-		if self._serial is None or self._connection_closing:
+		if self._serial is None:
 			return
 
 		self._log("Send: %s" % cmd)
