ebd1c2e14c8575680a65db88034fde7f4fbfecf2
Gina Häußge <osd@foosel.net>
2015-11-16 17:07:12 +0100

    Forgot flattr URL (which shouldn't be in there any longer anyhow)

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,15 +7,6 @@ $(function() {
   // make code pretty
   window.prettyPrint && prettyPrint();
 
-  // Flattr
-  (function() {
-      var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-      s.type = 'text/javascript';
-      s.async = true;
-      s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-      t.parentNode.insertBefore(s, t);
-  })();
-
   // G+
   (function() {
       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
