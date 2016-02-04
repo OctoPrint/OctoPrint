e68ec435ea3edfa44a10de6388470b22e6c01069
Gina Häußge <osd@foosel.net>
2016-02-04 13:54:03 +0100

    Made PipCaller compatible to pip 8.0.0
    pip 8 doesn't output ANY of the generated stdout output from
    setup.py anymore. So instead of transfering the info we need about
    a pip environment on stdout, we now need a temporary folder.
    
    Yay.

--- src/octoprint/util/piptestballoon/setup.py
+++ src/octoprint/util/piptestballoon/setup.py
@@ -1,10 +1,25 @@
-from setuptools import setup
+from __future__ import print_function
+import os
+import sys
 
-def run_checks():
+"""
+This "python package" doesn't actually install. This is intenional. It is merely
+used to figure out some information about the environment a specific pip call
+is running under (installation dir, whether it belongs to a virtual environment,
+whether the install location is writable by the current user), and for that it
+only needs to be invoked by pip, the pip call doesn't have to be successful
+however.
+
+If an environment variable "TESTBALLOON_OUTPUT" is set, it will be used as location
+to write a file with the figured out data to. Simply writing to stdout (the default
+behaviour if no such environment variable is set) is sadly not going to work out
+with versions of pip > 8.0.0, which capture all stdout output regardless of used
+--verbose or --log flags.
+"""
+
+def produce_output(stream):
 	from distutils.command.install import install as cmd_install
 	from distutils.dist import Distribution
-	import sys
-	import os
 
 	cmd = cmd_install(Distribution())
 	cmd.finalize_options()
@@ -13,18 +28,21 @@ def run_checks():
 	virtual_env = hasattr(sys, "real_prefix")
 	writable = os.access(install_dir, os.W_OK)
 
-	print("!!! PIP_INSTALL_DIR={}".format(install_dir))
-	print("!!! PIP_VIRTUAL_ENV={}".format(virtual_env))
-	print("!!! PIP_WRITABLE={}".format(writable))
-	sys.stdout.flush()
+	print("PIP_INSTALL_DIR={}".format(install_dir), file=stream)
+	print("PIP_VIRTUAL_ENV={}".format(virtual_env), file=stream)
+	print("PIP_WRITABLE={}".format(writable), file=stream)
+	stream.flush()
 
-def parameters():
-	run_checks()
 
-	return dict(
-		name="OctoPrint-PipTestBalloon",
-		version="1.0",
-		description="Just a test balloon to check a couple of pip related settings"
-	)
+path = os.environ.get("TESTBALLOON_OUTPUT", None)
+if path is not None:
+	# environment variable set, write to a log
+	path = os.path.abspath(path)
+	with open(path, mode="w+b") as output:
+		produce_output(output)
+else:
+	# write to stdout
+	produce_output(sys.stdout)
 
-setup(**parameters())
+# fail intentionally
+sys.exit(-1)
