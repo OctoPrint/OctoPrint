1a4c06d6f63f1d527577029123cc7cc9e2a70792
Gina Häußge <osd@foosel.net>
2016-03-18 14:59:13 +0100

    Added flag to trigger ok for resend requests
    That's necessary to keep communications going in case of a resend request
    for firmwares that do not send an ok after a resend request.

--- src/octoprint/static/js/app/viewmodels/settings.js
+++ src/octoprint/static/js/app/viewmodels/settings.js
@@ -141,6 +141,7 @@ $(function() {
         self.serial_ignoreErrorsFromFirmware = ko.observable(undefined);
         self.serial_disconnectOnErrors = ko.observable(undefined);
         self.serial_triggerOkForM29 = ko.observable(undefined);
+        self.serial_supportResendsWithoutOk = ko.observable(undefined);
 
         self.folder_uploads = ko.observable(undefined);
         self.folder_timelapse = ko.observable(undefined);
@@ -460,6 +461,7 @@ $(function() {
             self.serial_ignoreErrorsFromFirmware(response.serial.ignoreErrorsFromFirmware);
             self.serial_disconnectOnErrors(response.serial.disconnectOnErrors);
             self.serial_triggerOkForM29(response.serial.triggerOkForM29);
+            self.serial_supportResendsWithoutOk(response.serial.supportResendsWithoutOk);
 
             self.folder_uploads(response.folder.uploads);
             self.folder_timelapse(response.folder.timelapse);
@@ -551,7 +553,8 @@ $(function() {
                         "helloCommand": self.serial_helloCommand(),
                         "ignoreErrorsFromFirmware": self.serial_ignoreErrorsFromFirmware(),
                         "disconnectOnErrors": self.serial_disconnectOnErrors(),
-                        "triggerOkForM29": self.serial_triggerOkForM29()
+                        "triggerOkForM29": self.serial_triggerOkForM29(),
+                        "supportResendsWithoutOk": self.serial_supportResendsWithoutOk()
                     },
                     "folder": {
                         "uploads": self.folder_uploads(),
