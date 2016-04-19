49e5133d050a96b655f880fc9e0fdeed47327312
Gina Häußge <osd@foosel.net>
2016-04-19 14:54:45 +0200

    Some fixes and optimizations in design and wording

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,7 +2,24 @@ $(function() {
   // make code pretty
   window.prettyPrint && prettyPrint();
 
+  // init slick slide shows
   $(".slick-container").slick();
 
+  // init smooth scrolling
   smoothScroll.init();
+
+  // "to top" Button
+  var evalToTop = function() {
+    var offset = $(window).height() * 0.5;
+    var buttons = $(".to-top");
+    var duration = 200;
+
+    if ($(this).scrollTop() > offset) {
+      buttons.fadeIn(duration);
+    } else {
+      buttons.fadeOut(duration);
+    }
+  };
+  $(window).scroll(evalToTop);
+  evalToTop();
 });
