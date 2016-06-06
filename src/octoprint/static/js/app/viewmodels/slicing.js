4f3e4f7f385fce0ce88157e3b6298b0c46766217
Gina Häußge <osd@foosel.net>
2016-06-06 16:20:47 +0200

    Force rescanning of available slicing profiles for slicer on update
    Should make newly uploaded profiles available in the slicing dialog without
    having to first deselect and then reselected the current slicer.

--- src/octoprint/static/js/app/viewmodels/slicing.js
+++ src/octoprint/static/js/app/viewmodels/slicing.js
@@ -102,6 +102,7 @@ $(function() {
 
             if (selectedSlicer != undefined) {
                 self.slicer(selectedSlicer);
+                self.profilesForSlicer(selectedSlicer);
             }
 
             self.defaultSlicer = selectedSlicer;
