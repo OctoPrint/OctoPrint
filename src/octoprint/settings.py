3614033b0df60cd2bc44fe4c97ee9d275f05a81e
Gina Häußge <osd@foosel.net>
2016-07-21 16:10:00 +0200

    No endless circles if we can't write any bytes to serial

--- src/octoprint/settings.py
+++ src/octoprint/settings.py
@@ -85,6 +85,7 @@ default_settings = {
 			"temperature": 5,
 			"sdStatus": 1
 		},
+		"maxWritePasses": 5,
 		"additionalPorts": [],
 		"longRunningCommands": ["G4", "G28", "G29", "G30", "G32", "M400", "M226"],
 		"checksumRequiringCommands": ["M110"],
