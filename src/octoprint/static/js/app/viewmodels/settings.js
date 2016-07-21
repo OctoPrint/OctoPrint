9eb1485e8459974d8f5d856b5c33f8195fa7c37f
Gina Häußge <osd@foosel.net>
2016-07-21 18:46:54 +0200

    Add max timeouts configuration to settings

--- src/octoprint/static/js/app/viewmodels/settings.js
+++ src/octoprint/static/js/app/viewmodels/settings.js
@@ -144,6 +144,9 @@ $(function() {
         self.serial_disconnectOnErrors = ko.observable(undefined);
         self.serial_triggerOkForM29 = ko.observable(undefined);
         self.serial_supportResendsWithoutOk = ko.observable(undefined);
+        self.serial_maxTimeoutsIdle = ko.observable(undefined);
+        self.serial_maxTimeoutsPrinting = ko.observable(undefined);
+        self.serial_maxTimeoutsLong = ko.observable(undefined);
 
         self.folder_uploads = ko.observable(undefined);
         self.folder_timelapse = ko.observable(undefined);
@@ -464,6 +467,9 @@ $(function() {
             self.serial_disconnectOnErrors(response.serial.disconnectOnErrors);
             self.serial_triggerOkForM29(response.serial.triggerOkForM29);
             self.serial_supportResendsWithoutOk(response.serial.supportResendsWithoutOk);
+            self.serial_maxTimeoutsIdle(response.serial.maxTimeoutsIdle);
+            self.serial_maxTimeoutsPrinting(response.serial.maxTimeoutsPrinting);
+            self.serial_maxTimeoutsLong(response.serial.maxTimeoutsLong);
 
             self.folder_uploads(response.folder.uploads);
             self.folder_timelapse(response.folder.timelapse);
@@ -556,7 +562,10 @@ $(function() {
                         "ignoreErrorsFromFirmware": self.serial_ignoreErrorsFromFirmware(),
                         "disconnectOnErrors": self.serial_disconnectOnErrors(),
                         "triggerOkForM29": self.serial_triggerOkForM29(),
-                        "supportResendsWithoutOk": self.serial_supportResendsWithoutOk()
+                        "supportResendsWithoutOk": self.serial_supportResendsWithoutOk(),
+                        "maxTimeoutsIdle": self.serial_maxTimeoutsIdle(),
+                        "maxTimeoutsPrinting": self.serial_maxTimeoutsPrinting(),
+                        "maxTimeoutsLong": self.serial_maxTimeoutsLong()
                     },
                     "folder": {
                         "uploads": self.folder_uploads(),
