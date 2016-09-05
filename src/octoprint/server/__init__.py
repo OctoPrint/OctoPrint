b48af7133ed46a2e4a40c36028b792de19c5c872
Gina Häußge <osd@foosel.net>
2016-09-05 12:10:58 +0200

    Fixed validator for tornado

--- src/octoprint/server/__init__.py
+++ src/octoprint/server/__init__.py
@@ -357,7 +357,7 @@ class Server(object):
 			allow_client_caching=False
 		)
 		additional_mime_types=dict(mime_type_guesser=mime_type_guesser)
-		admin_validator = dict(access_validation=util.tornado.access_validation_factory(app, loginManager, util.flask.user_validator))
+		admin_validator = dict(access_validation=util.tornado.access_validation_factory(app, loginManager, util.flask.admin_validator))
 		no_hidden_files_validator = dict(path_validation=util.tornado.path_validation_factory(lambda path: not octoprint.util.is_hidden_path(path), status_code=404))
 
 		def joined_dict(*dicts):
