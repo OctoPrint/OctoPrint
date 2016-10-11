343631b3c6ce63776be3b58c3b557fa2baa3a214
Gina Häußge <osd@foosel.net>
2016-10-11 10:53:37 +0200

    Custom SerialLoggingHandler
    Does roll overs on next connections to the printer

--- src/octoprint/printer/standard.py
+++ src/octoprint/printer/standard.py
@@ -194,6 +194,10 @@ class Printer(PrinterInterface, comm.MachineComPrintCallback):
 		if self._comm is not None:
 			self._comm.close()
 		self._printerProfileManager.select(profile)
+
+		from octoprint.logging.handlers import SerialLogHandler
+		SerialLogHandler.on_open_connection()
+
 		self._comm = comm.MachineCom(port, baudrate, callbackObject=self, printerProfileManager=self._printerProfileManager)
 
 	def disconnect(self):
