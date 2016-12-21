f2bd5d605e77a585b5ff1951791b500466138e42
Marc Hannappel <hannappel@uni-wuppertal.de>
2016-12-21 10:51:19 +0100

    Added active tab using hash from URL feature, as requested in issue #203
    (cherry picked from commit bcb74d0)

--- src/octoprint/static/js/app/main.js
+++ src/octoprint/static/js/app/main.js
@@ -539,6 +539,15 @@ $(function() {
         // reload overlay
         $("#reloadui_overlay_reload").click(function() { location.reload(); });
 
+        var changeTab = function()
+        {
+            var hashtag = window.location.hash;
+
+            var tab = $('#tabs a[href="' + hashtag + '"]');
+            tab.tab("show");
+            onTabChange(hashtag);
+        };
+
         //~~ final initialization - passive login, settings fetch, view model binding
 
         if (!_.has(viewModelMap, "settingsViewModel")) {
@@ -653,6 +662,14 @@ $(function() {
                 log.debug("Browser tab is now " + (status ? "visible" : "hidden"));
                 callViewModels(allViewModels, "onBrowserTabVisibilityChange", [status]);
             });
+
+            $(window).on("hashchange", function() {
+                changeTab();
+            });
+
+            if (window.location.hash !== "") {
+                changeTab();
+            }
 
             log.info("Application startup complete");
         };
