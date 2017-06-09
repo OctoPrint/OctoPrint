f53be57f0d4dac316db58b93a5a12be258d0d391
Marc Hannappel <hannappel@uni-wuppertal.de>
2017-06-09 09:31:50 +0200

    Added User wrapper, this makes sure we stay backwards compatible

--- src/octoprint/users.py
+++ src/octoprint/users.py
@@ -8,3 +8,22 @@ __copyright__ = "Copyright (C) 2017 The OctoPrint Project - Released under terms
 # Wrapper to the new access.users location
 
 from octoprint.access.users import *
+
+AccessUser = User
+
+import warnings
+warnings.warn("octoprint.users is deprecated, use octoprint.access.users instead", DeprecationWarning, stacklevel=2)
+
+
+class User(AccessUser):
+	@deprecated("octoprint.users.User is deprecated, please use octoprint.access.users.User instead")
+	def __init__(self, username, passwordHash, active, roles, apikey=None, settings=None):
+		from octoprint.server import groupManager
+		groups = [groupManager.guests_group]
+		if "user" is None:
+			groups = [groupManager.find_group("Users")]
+		if "admin" in roles:
+			groups = [groupManager.admins_group]
+
+		AccessUser(username=username, passwordHash=passwordHash, active=active, permissions=None, groups=groups,
+		           apikey=apikey, settings=settings)
