d72ae6102b7efd895a99c1ea78f34dd2fa0a467b
Marc Hannappel <hannappel@uni-wuppertal.de>
2017-07-05 10:07:20 +0200

    Added missing files/folders

---
+++ tests/access/_plugins/permissions_plugin.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+
+def plugin_permissions(components):
+	return [
+		dict(name="fancy permission", description="My Fancy new Permission", roles=["fancy"]),
+		dict(name="fancy permission with two roles", description="My Fancy new Permission with two roles", roles=["fancy1", "fancy2"])
+	]
+
+
+__plugin_name__ = "Permissions Plugin"
+__plugin_description__ = "Test permissions plugin"
+__plugin_hooks__ = {
+	'octoprint.access.permissions': plugin_permissions,
+}
