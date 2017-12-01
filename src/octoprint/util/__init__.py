f72dfb48e1acf61110227f10271b90d673eb1c0f
Gina Häußge <gina@octoprint.org>
2017-12-01 18:13:35 +0100

    Fix ordering of plugin assets
    Should be alphabetical per their identifier. As noticed by @BillyBlaze

--- src/octoprint/util/__init__.py
+++ src/octoprint/util/__init__.py
@@ -20,6 +20,7 @@ import threading
 from functools import wraps
 import warnings
 import contextlib
+import collections
 
 try:
 	import queue
@@ -735,6 +736,51 @@ def dict_filter(dictionary, filter_function):
 	assert isinstance(dictionary, dict)
 	assert callable(filter_function)
 	return dict((k, v) for k, v in dictionary.items() if filter_function(k, v))
+
+
+# Source: http://stackoverflow.com/a/6190500/562769
+class DefaultOrderedDict(collections.OrderedDict):
+	def __init__(self, default_factory=None, *a, **kw):
+
+		if default_factory is not None and not callable(default_factory):
+			raise TypeError('first argument must be callable')
+		collections.OrderedDict.__init__(self, *a, **kw)
+		self.default_factory = default_factory
+
+	def __getitem__(self, key):
+		try:
+			return collections.OrderedDict.__getitem__(self, key)
+		except KeyError:
+			return self.__missing__(key)
+
+	def __missing__(self, key):
+		if self.default_factory is None:
+			raise KeyError(key)
+		self[key] = value = self.default_factory()
+		return value
+
+	def __reduce__(self):
+		if self.default_factory is None:
+			args = tuple()
+		else:
+			args = self.default_factory,
+		return type(self), args, None, None, self.items()
+
+	def copy(self):
+		return self.__copy__()
+
+	def __copy__(self):
+		return type(self)(self.default_factory, self)
+
+	def __deepcopy__(self, memo):
+		import copy
+		return type(self)(self.default_factory,
+		                  copy.deepcopy(self.items()))
+
+	# noinspection PyMethodOverriding
+	def __repr__(self):
+		return 'OrderedDefaultDict(%s, %s)' % (self.default_factory,
+		                                       collections.OrderedDict.__repr__(self))
 
 
 class Object(object):
