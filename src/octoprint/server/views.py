9f1abee023618515f1a1b801ee014f1f281f4931
Gina Häußge <gina@octoprint.org>
2018-02-06 09:30:13 +0100

    Add refresh button to the connection panel

--- src/octoprint/server/views.py
+++ src/octoprint/server/views.py
@@ -473,7 +473,7 @@ def _process_templates():
 		else:
 			# Finally everything else
 			return u"2:{}".format(to_unicode(d[0]))
-	
+
 	template_sorting = dict(
 		navbar=dict(add="prepend", key=None),
 		sidebar=dict(add="append", key="name"),
@@ -535,7 +535,7 @@ def _process_templates():
 	# sidebar
 
 	templates["sidebar"]["entries"]= dict(
-		connection=(gettext("Connection"), dict(template="sidebar/connection.jinja2", _div="connection", icon="signal", styles_wrapper=["display: none"], data_bind="visible: loginState.isUser")),
+		connection=(gettext("Connection"), dict(template="sidebar/connection.jinja2", _div="connection", icon="signal", styles_wrapper=["display: none"], data_bind="visible: loginState.isUser", template_header="sidebar/connection_header.jinja2")),
 		state=(gettext("State"), dict(template="sidebar/state.jinja2", _div="state", icon="info-circle")),
 		files=(gettext("Files"), dict(template="sidebar/files.jinja2", _div="files", icon="list", classes_content=["overflow_visible"], template_header="sidebar/files_header.jinja2"))
 	)
