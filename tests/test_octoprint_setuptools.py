025f545ef7ea41a34d2307278ecc13e3410d1ff2
Gina Häußge <gina@octoprint.org>
2018-04-06 14:58:47 +0200

    Better requirement detection in octoprint_setuptools
    Plus unit tests.

---
+++ tests/test_octoprint_setuptools.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+import unittest
+import ddt
+
+import octoprint_setuptools
+
+@ddt.ddt
+class OctoPrintSetuptoolsTest(unittest.TestCase):
+
+	@ddt.data(
+		("OctoPrint", ["OctoPrint", "flask"], True),
+		("OctoPrint", ["OctoPrint<1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint<=1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint==1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint!=1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint>=1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint>1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint~=1.3.7", "flask"], True),
+		("OctoPrint", ["OctoPrint===1.3.7", "flask"], True),
+		("OctoPrint", ["oCTOpRINT>=1.3.7", "flask"], True),
+		("OctoPrint", ["flask"], False),
+		("OctoPrint", [], False),
+		("OctoPrint", None, False),
+		(None, [], False),
+	)
+	@ddt.unpack
+	def test_has_requirement(self, requirement, requirements, expected):
+		actual = octoprint_setuptools.has_requirement(requirement, requirements)
+		self.assertEqual(actual, expected)
