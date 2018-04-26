2a0eda5c3406b8c7b90e5616f60a87381cac857d
Gina Häußge <gina@octoprint.org>
2018-04-26 11:30:48 +0200

    Make sure we have the current user data in editor
    Fixes #2534

--- src/octoprint/static/js/app/viewmodels/usersettings.js
+++ src/octoprint/static/js/app/viewmodels/usersettings.js
@@ -25,7 +25,7 @@ $(function() {
             self.access_apikey(undefined);
             self.interface_language("_default");
 
-            if (newUser != undefined) {
+            if (newUser !== undefined) {
                 self.access_apikey(newUser.apikey);
                 if (newUser.settings.hasOwnProperty("interface") && newUser.settings.interface.hasOwnProperty("language")) {
                     self.interface_language(newUser.settings.interface.language);
@@ -34,18 +34,30 @@ $(function() {
         });
 
         self.passwordMismatch = ko.pureComputed(function() {
-            return self.access_password() != self.access_repeatedPassword();
+            return self.access_password() !== self.access_repeatedPassword();
         });
 
         self.show = function(user) {
             if (!CONFIG_ACCESS_CONTROL) return;
 
-            if (user == undefined) {
+            if (user === undefined) {
                 user = self.loginState.currentUser();
             }
 
-            self.currentUser(user);
-            self.userSettingsDialog.modal("show");
+            var process = function(user) {
+                self.currentUser(user);
+                self.userSettingsDialog.modal("show");
+            };
+
+            // make sure we have the current user data, see #2534
+            OctoPrint.users.get(user.name)
+                .done(function(data) {
+                    process(data);
+                })
+                .fail(function() {
+                    log.warn("Could not fetch current user data, proceeding with client side data copy");
+                    process(user);
+                });
         };
 
         self.save = function() {
