faff20943fa1a983f2d50b7f7646fd04de703c35
Gina Häußge <gina@octoprint.org>
2018-07-05 10:58:45 +0200

    Capture & log any errors in the state update worker
    See #2715

--- src/octoprint/printer/standard.py
+++ src/octoprint/printer/standard.py
@@ -1305,20 +1305,23 @@ class StateMonitor(object):
 		self._change_event.set()
 
 	def _work(self):
-		while True:
-			self._change_event.wait()
+		try:
+			while True:
+				self._change_event.wait()
 
-			now = time.time()
-			delta = now - self._last_update
-			additional_wait_time = self._interval - delta
-			if additional_wait_time > 0:
-				time.sleep(additional_wait_time)
+				now = time.time()
+				delta = now - self._last_update
+				additional_wait_time = self._interval - delta
+				if additional_wait_time > 0:
+					time.sleep(additional_wait_time)
 
-			with self._state_lock:
-				data = self.get_current_data()
-				self._update_callback(data)
-				self._last_update = time.time()
-				self._change_event.clear()
+				with self._state_lock:
+					data = self.get_current_data()
+					self._update_callback(data)
+					self._last_update = time.time()
+					self._change_event.clear()
+		except:
+			logging.getLogger(__name__).exception("Looks like something crashed inside the state update worker. Please report this on the OctoPrint issue tracker (make sure to include logs!)")
 
 	def get_current_data(self):
 		with self._progress_lock:
