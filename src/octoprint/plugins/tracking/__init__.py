53a2e0450107a8d90fed771b9a59c70295a41b51
Gina Häußge <gina@octoprint.org>
2018-07-17 14:59:10 +0200

    Tracking: adapt to new pi support plugin

--- src/octoprint/plugins/tracking/__init__.py
+++ src/octoprint/plugins/tracking/__init__.py
@@ -129,9 +129,10 @@ class TrackingPlugin(octoprint.plugin.SettingsPlugin,
 		               freq=self._environment[b"hardware"][b"freq"],
 		               ram=self._environment[b"hardware"][b"ram"])
 
-		if b"plugins" in self._environment and b"octopi_support" in self._environment[b"plugins"]:
-			payload[b"octopi_version"] = self._environment[b"plugins"][b"octopi_support"][b"version"]
-			payload[b"pi_model"] = self._environment[b"plugins"][b"octopi_support"][b"model"]
+		if b"plugins" in self._environment and b"pi_support" in self._environment[b"plugins"]:
+			payload[b"pi_model"] = self._environment[b"plugins"][b"pi_support"][b"model"]
+			if b"octopi_version" in self._environment[b"plugin"][b"pi_support"]:
+				payload[b"octopi_version"] = self._environment[b"plugins"][b"octopi_support"][b"octopi_version"]
 
 		self._track("startup", **payload)
 
