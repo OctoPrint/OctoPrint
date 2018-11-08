OctoPrint/OctoPrint

commit 4caf4ef09b402111b7abf36425315271ce588108

Fix GCODE viewer not being able to load files
Side effect of the ForcedLogin plugin and a bug in
get_flask_user_from_request.

Closes #2873

src/octoprint/server/util/flask.py
@@ -1109,7 +1109,8 @@ def get_flask_user_from_request(request):
 	apikey = octoprint.server.util.get_api_key(request)
 	if settings().getBoolean(["api", "enabled"]) and apikey is not None:
 		user = octoprint.server.util.get_user_for_apikey(apikey)
-	else:
+
+	if user is None:
 		user = flask_login.current_user
 
 	return user

commit 1667f9bd520eeb5d6b8700686ba4ff5bcc07320e

Fix bug introduced in 4caf4ef09

src/octoprint/server/util/flask.py
@@ -1106,6 +1106,8 @@ def get_flask_user_from_request(request):
 	import flask_login
 	from octoprint.settings import settings
 
+	user = None
+
 	apikey = octoprint.server.util.get_api_key(request)
 	if settings().getBoolean(["api", "enabled"]) and apikey is not None:
 		user = octoprint.server.util.get_user_for_apikey(apikey)