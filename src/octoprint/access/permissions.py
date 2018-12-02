94013ddb70482318a190aec30700d5dd550ca95d
MirceaDan <ByReaL@users.noreply.github.com>
2018-12-02 01:25:27 -0800

    py23 compat for defining metaclasses
    in python 3 and py2 metaclases are define ddiferentley, using six to get to a common solution

--- src/octoprint/access/permissions.py
+++ src/octoprint/access/permissions.py
@@ -5,6 +5,7 @@ __author__ = "Marc Hannappel <salandora@gmail.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
 __copyright__ = "Copyright (C) 2017 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
+import six
 
 from flask import g, abort
 from flask_babel import gettext
@@ -249,8 +250,8 @@ class PermissionsMetaClass(type):
 		return None
 
 
+@six.add_metaclass(PermissionsMetaClass)
 class Permissions(object):
-	__metaclass__ = PermissionsMetaClass
 
 	# Special permission
 	ADMIN                  = OctoPrintPermission("Admin",
