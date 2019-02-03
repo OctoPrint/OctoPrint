cd6ae6e47b59df9996ee11926d9b7644761efb08
Matthias Urlichs <matthias@urlichs.de>
2019-02-03 16:35:48 +0100

    setup 2 unicode_literals

--- src/octoprint_setuptools/__init__.py
+++ src/octoprint_setuptools/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
+# NO unicode_literals because Py2 setuptool can't cope with them
 
-__author__ = "Gina Häußge <osd@foosel.net>"
-__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
-__copyright__ = "Copyright (C) 2015 The OctoPrint Project - Released under terms of the AGPLv3 License"
+__author__ = u"Gina Häußge <osd@foosel.net>"
+__license__ = u'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
+__copyright__ = u"Copyright (C) 2015 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
 import os
 import shutil
