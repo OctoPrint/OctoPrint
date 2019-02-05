d7d3272905ae4381cf60dbb216c87efcbafe67dd
Matthias Urlichs <matthias@urlichs.de>
2019-02-05 17:43:31 +0100

    Merge errors fixed

--- src/octoprint/server/util/flask.py
+++ src/octoprint/server/util/flask.py
@@ -595,8 +595,9 @@ def passive_login():
 			and settings().get(["accessControl", "localNetworks"]) is not None:
 
 		autologin_as = settings().get(["accessControl", "autologinAs"])
-
 		local_networks = _local_networks()
+		logger.debug("Checking if remote address {} is in localNetworks ({!r})".format(remote_address, local_networks))
+
 		try:
 			if netaddr.IPAddress(remote_address) in local_networks:
 				autologin_user = octoprint.server.userManager.findUser(autologin_as)
@@ -605,23 +606,6 @@ def passive_login():
 
 					logger.info("Passively logging in user {} from {} via autologin".format(user.get_id(), local_networks))
 					user = login(autologin_user)
-			if netaddr.IPAddress(remote_address) in local_networks:
-				user = octoprint.server.userManager.findUser(autologin_as)
-				if user is not None and user.is_active():
-					user = octoprint.server.userManager.login_user(user)
-					flask.session["usersession.id"] = user.session
-					flask.g.user = user
-					flask_login.login_user(user)
-					flask_principal.identity_changed.send(flask.current_app._get_current_object(),
-					                                      identity=flask_principal.Identity(user.get_id()))
-
-					logger.info("Passively logging in user {} from {} via autologin".format(user.get_id(),
-					                                                                        remote_address))
-
-					response = user.asDict()
-					response["_is_external_client"] = ip_check_enabled and not is_lan_address(remote_address,
-					                                                                          additional_private=ip_check_trusted)
-					return flask.jsonify(response)
 		except Exception:
 			logger.exception("Could not autologin user {} for networks {}".format(autologin_as, local_networks))
 
