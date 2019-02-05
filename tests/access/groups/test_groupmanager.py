becd16b892867d6230c6f31a6219cb9aca3a8658
Gina Häußge <gina@octoprint.org>
2019-02-05 16:08:40 +0100

    🐛 Fix a test

--- tests/access/groups/test_groupmanager.py
+++ tests/access/groups/test_groupmanager.py
@@ -33,7 +33,9 @@ def group_manager_with_temp_file():
 class GroupManagerTestCase(unittest.TestCase):
 	def test_add_remove_group(self):
 		with group_manager_with_temp_file() as group_manager:
-			group_manager.add_group("fancy", "Fancy Group", "My Fancy New Group", permissions=[TEST_PERMISSION_1],
+			group_manager.add_group("fancy", "Fancy Group", "My Fancy New Group",
+			                        permissions=[TEST_PERMISSION_1],
+			                        subgroups=[],
 			                        save=False)
 			self.assertIsNotNone(group_manager.find_group("fancy"))
 
