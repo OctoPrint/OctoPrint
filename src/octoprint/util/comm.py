6e0024149edb7ffa9fbccdc6ffa89435ee6e3bf9
Daniel Joyce <daniel.a.joyce@gmail.com>
2019-02-07 07:36:11 -0800

    Add support for klipper firmware autodetect

--- src/octoprint/util/comm.py
+++ src/octoprint/util/comm.py
@@ -1861,6 +1861,11 @@ class MachineCom(object):
 
 							disable_external_heatup_detection = True # see #2854
 
+						elif "klipper" in firmware_name.lower():
+							self._logger.info("Detected Klipper firmware, enabling relevant features for issue free communication")
+
+							self._unknownCommandsNeedAck = True
+
 						self._firmware_info_received = True
 						self._firmware_info = data
 						self._firmware_name = firmware_name
