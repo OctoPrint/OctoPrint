27e1f2e6e7a0d1686c06b2e310ac4797e2a3fe13
Gina Häußge <gina@octoprint.org>
2019-02-13 15:26:15 +0100

    🐛 Fix package collection
    As mentioned in #2722
    
    See also razerraz@d269c36

--- src/octoprint_setuptools/__init__.py
+++ src/octoprint_setuptools/__init__.py
@@ -529,7 +529,10 @@ def create_plugin_setup_parameters(identifier="todo", name="TODO", version="0.1"
 	cmdclass.update(get_babel_commandclasses(pot_file=pot_file, output_dir=translation_dir, bundled_dir=bundled_dir, pack_name_prefix="{name}-i18n-".format(**locals()), pack_path_prefix="_plugins/{identifier}/".format(**locals())))
 
 	from setuptools import find_packages
-	packages = list(set([package] + list(filter(lambda x: x.startswith("{package}.".format(package=package)), find_packages(where=source_folder, exclude=ignored_packages)) + additional_packages)))
+	packages = set([package]
+	               + list(filter(lambda x: x.startswith("{package}.".format(package=package)),
+	                             find_packages(where=source_folder, exclude=ignored_packages)))
+	               + additional_packages)
 	print("Found packages: {packages!r}".format(**locals()))
 
 	return dict(
