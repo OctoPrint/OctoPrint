94ae90ab80626f82b7e3f1ae8955639b8b62aaa0
Gina Häußge <gina@octoprint.org>
2019-12-12 12:43:45 +0100

    ♻ Cleaned up a pre-py3 leftover comment

--- src/octoprint/__main__.py
+++ src/octoprint/__main__.py
@@ -2,10 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-#import sys
-#if sys.version_info[0] >= 3:
-#	raise RuntimeError("Sorry, OctoPrint does not yet support Python 3")
-
 if __name__ == "__main__":
 	import octoprint
 	octoprint.main()
