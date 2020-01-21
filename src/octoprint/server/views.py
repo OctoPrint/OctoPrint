OctoPrint/OctoPrint

commit 3f3cb556a703f15c60907956e66f8b0abdef6a5d

🐛 Fix guest access to settings
Needed its own permission, which in turn needed some changes in how the
whole "guest without read permission" situation is handled. I've
reintroduced a custom login view since the main UI would not be able
to fully load up without settings access. Said login view will override
the default UI view if the account in question does not have
Permissions.STATUS and also Permissions.SETTINGS_READ. It pretty much is
the forcelogin plugin from 1.3.x.

Closes #3425

src/octoprint/server/views.py
@@ -367,7 +367,7 @@ def index():
 
 		return view(now, request, render_kwargs)
 
-	def default_view():
+	def ui_view():
 		filtered_templates = _filter_templates(_templates[locale], default_template_filter)
 
 		wizard = wizard_active(filtered_templates)
@@ -406,6 +406,15 @@ def index():
 		                                                   dict(),
 		                                                   dict())
 		return preemptively_cached()
+
+	def login_view():
+		return util.flask.add_non_caching_response_headers(make_response(render_template("forcelogin.jinja2")))
+
+	def default_view():
+		if Permissions.STATUS.can() and Permissions.SETTINGS_READ.can():
+			return ui_view()
+		else:
+			return login_view()
 
 	response = None
 

src/octoprint/access/permissions.py
@@ -275,10 +275,11 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	                                             default_groups=[ADMIN_GROUP])
 
 	STATUS                 = OctoPrintPermission("Status",
-	                                             gettext("Allows to gather status information, e.g. job progress, "
-	                                                     "printer state, temperatures, ..."),
+	                                             gettext("Allows to gather basic status information, e.g. job progress, "
+	                                                     "printer state, temperatures, ... Mandatory for the default UI "
+	                                                     "to work"),
 	                                             RoleNeed("status"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 
 	CONNECTION             = OctoPrintPermission("Connection",
 	                                             gettext("Allows to connect to and disconnect from a printer"),
@@ -288,7 +289,7 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	WEBCAM                 = OctoPrintPermission("Webcam",
 	                                             gettext("Allows to watch the webcam stream"),
 	                                             RoleNeed("webcam"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 
 	SYSTEM                 = OctoPrintPermission("System",
 	                                             gettext("Allows to run system commands, e.g. restart OctoPrint, "
@@ -300,7 +301,7 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	                                             gettext("Allows to retrieve a list of all uploaded files, including"
 	                                                     "their metadata (e.g. date, file size, analysis results, ...)"),
 	                                             RoleNeed("files_list"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 	FILES_UPLOAD           = OctoPrintPermission("File Upload",
 	                                             gettext("Allows users to upload new files"),
 	                                             RoleNeed("files_upload"),
@@ -309,7 +310,7 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	                                             gettext("Allows users to download files. The GCODE viewer is "
 	                                                     "affected by this as well."),
 	                                             RoleNeed("files_download"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 	FILES_DELETE           = OctoPrintPermission("File Delete",
 	                                             gettext("Allows users to delete files"),
 	                                             RoleNeed("files_delete"),
@@ -328,13 +329,13 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	                                             gettext("Allows access to the GCODE viewer if the \"File Download\""
 	                                                     "permission is also set."),
 	                                             RoleNeed("gcodeviewer"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 
 	MONITOR_TERMINAL       = OctoPrintPermission("Terminal",
 	                                             gettext("Allows to watch the terminal tab but not to send commands "
 	                                                     "to the printer from it"),
 	                                             RoleNeed("monitor_terminal"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 
 	CONTROL                = OctoPrintPermission("Control",
 	                                             gettext("Allows to control of the printer by using the temperature controls,"
@@ -350,11 +351,11 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	TIMELAPSE_LIST         = OctoPrintPermission("Timelapse List",
 	                                             gettext("Allows to list timelapse videos"),
 	                                             RoleNeed("timelapse_list"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 	TIMELAPSE_DOWNLOAD     = OctoPrintPermission("Timelapse Download",
 	                                             gettext("Allows to download timelapse videos"),
 	                                             RoleNeed("timelapse_download"),
-	                                             default_groups=[USER_GROUP, READONLY_GROUP])
+	                                             default_groups=[USER_GROUP])
 	TIMELAPSE_DELETE       = OctoPrintPermission("Timelapse Delete",
 	                                             gettext("Allows to delete timelapse videos and unrendered timelapses"),
 	                                             RoleNeed("timelapse_delete"),
@@ -366,8 +367,13 @@ class Permissions(with_metaclass(PermissionsMetaClass)):
 	                                             RoleNeed("timelapse_admin"), TIMELAPSE_LIST, TIMELAPSE_DOWNLOAD,
 	                                             default_groups=[USER_GROUP])
 
-	SETTINGS               = OctoPrintPermission("Settings",
-	                                             gettext("Allows to manage settings"),
+	SETTINGS_READ          = OctoPrintPermission("Settings Access",
+	                                             gettext("Allows to read non sensitive settings. Mandatory for the "
+	                                                     "default UI to work."),
+	                                             RoleNeed("settings_read"),
+	                                             default_groups=[USER_GROUP])
+	SETTINGS               = OctoPrintPermission("Settings Admin",
+	                                             gettext("Allows to manage settings and also to read sensitive settings"),
 	                                             RoleNeed("settings"),
 	                                             dangerous=True)
 

src/octoprint/static/forcelogin/css/forcelogin.css
@@ -1,0 +1,1 @@
+body{padding-top:40px;padding-bottom:40px;background-color:#f5f5f5}.form-signin{max-width:300px;padding:19px 29px 29px;margin:0 auto 20px;background-color:#fff;border:1px solid #e5e5e5;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;-webkit-box-shadow:0 1px 2px rgba(0,0,0,.05);-moz-box-shadow:0 1px 2px rgba(0,0,0,.05);box-shadow:0 1px 2px rgba(0,0,0,.05)}.form-signin .form-signin-heading,.form-signin .checkbox{margin-bottom:10px}.form-signin input[type=text],.form-signin input[type=password]{font-size:16px;height:auto;margin-bottom:15px;padding:7px 9px}#login-error,#login-offline{display:none}#login-error.in,#login-offline.in{display:block}#login-overlay{position:fixed;top:0;left:0;width:100%;height:100%;z-index:10000;display:none}#login-overlay.in{display:block}#login-overlay .background{position:fixed;top:0;left:0;width:100%;height:100%;background-color:#000;filter:alpha(opacity=50);-moz-opacity:.5;-khtml-opacity:.5;opacity:.5}#login-overlay .wrapper{position:absolute;top:0;bottom:0;left:0;right:0}#login-overlay .wrapper .outer{display:table;width:100%;height:100%}#login-overlay .wrapper .outer .inner{display:table-cell;vertical-align:middle}#login-overlay .wrapper .outer .inner .content{text-align:center;color:#fff}

src/octoprint/static/forcelogin/js/forcelogin.js
@@ -1,0 +1,71 @@
+/*
+ * Will get included into the login dialog, NOT into the regular OctoPrint
+ * web application.
+ */
+
+$(function() {
+    var overlayElement = $("#login-overlay");
+    var errorElement = $("#login-error");
+    var offlineElement = $("#login-offline");
+    var buttonElement = $("#login-button");
+    var reconnectElement = $("#login-reconnect");
+
+    buttonElement.click(function() {
+        var usernameElement = $("#login-user");
+        var passwordElement = $("#login-password");
+        var rememberElement = $("#login-remember");
+
+        var username = usernameElement.val();
+        var password = passwordElement.val();
+        var remember = rememberElement.prop("checked");
+
+        overlayElement.addClass("in");
+        errorElement.removeClass("in");
+
+        OctoPrint.browser.login(username, password, remember)
+            .done(function() {
+                location.reload();
+            })
+            .fail(function() {
+                usernameElement.val("");
+                passwordElement.val("");
+
+                overlayElement.removeClass("in");
+                errorElement.addClass("in");
+            });
+
+        return false;
+    });
+
+    var OctoPrint = window.OctoPrint;
+
+    OctoPrint.options.baseurl = BASE_URL;
+
+    var offlineTimer = undefined;
+    var clearOfflineTimer = function() {
+        if (offlineTimer !== undefined) {
+            window.clearTimeout(offlineTimer);
+            offlineTimer = undefined;
+        }
+    };
+
+    OctoPrint.socket.onConnected = function() {
+        clearOfflineTimer();
+        buttonElement.prop("disabled", false);
+        offlineElement.removeClass("in");
+    };
+
+    OctoPrint.socket.onDisconnected = function() {
+        clearOfflineTimer();
+        offlineTimer = window.setTimeout(function() {
+            buttonElement.prop("disabled", true);
+            offlineElement.addClass("in");
+        }, 1000);
+    };
+
+    reconnectElement.click(function() {
+        OctoPrint.socket.reconnect();
+    });
+
+    OctoPrint.socket.connect();
+});

src/octoprint/static/forcelogin/less/forcelogin.less
@@ -1,0 +1,90 @@
+body {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  max-width: 300px;
+  padding: 19px 29px 29px;
+  margin: 0 auto 20px;
+  background-color: #fff;
+  border: 1px solid #e5e5e5;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+  -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+  box-shadow: 0 1px 2px rgba(0,0,0,.05);
+
+  .form-signin-heading, .checkbox {
+    margin-bottom: 10px;
+  }
+
+  input[type="text"], input[type="password"] {
+    font-size: 16px;
+    height: auto;
+    margin-bottom: 15px;
+    padding: 7px 9px;
+  }
+}
+
+#login-error,
+#login-offline {
+  display: none;
+
+  &.in {
+    display: block;
+  }
+}
+
+#login-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10000;
+  display: none;
+
+  &.in {
+    display: block;
+  }
+
+  .background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000000;
+    filter:alpha(opacity=50);
+    -moz-opacity:0.5;
+    -khtml-opacity: 0.5;
+    opacity: 0.5;
+  }
+
+  .wrapper {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    .outer {
+      display: table;
+      width: 100%;
+      height: 100%;
+
+      .inner {
+        display: table-cell;
+        vertical-align: middle;
+
+        .content {
+          text-align: center;
+          color: white;
+        }
+      }
+    }
+  }
+}

src/octoprint/static/js/app/viewmodels/uistate.js
@@ -8,7 +8,7 @@ $(function() {
         self.loading = ko.observable(CONFIG_LOADINGANIMATION);
         self.loading_error = ko.observable(false);
         self.needlogin = ko.computed(function() {
-            return !self.loading() && !self.loginState.hasPermissionKo(self.access.permissions.STATUS)();
+            return !self.loading() && !self.loginState.hasAllPermissionsKo(self.access.permissions.STATUS, self.access.permissions.SETTINGS_READ)();
         });
         self.visible = ko.pureComputed(function() {
             return !self.loading() && !self.needlogin();
@@ -38,6 +38,12 @@ $(function() {
                         }
                     }
                 });
+        };
+
+        self.onUserLoggedOut = function() {
+            if (!self.loginState.hasAllPermissions(self.access.permissions.STATUS, self.access.permissions.SETTINGS_READ)) {
+                location.reload();
+            }
         };
 
         self.showLoadingError = function(error) {

src/octoprint/templates/forcelogin.jinja2
@@ -1,0 +1,70 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>OctoPrint Login</title>
+
+    <link rel="shortcut icon" href="{{ url_for('static', filename='img/tentacle-32x32.png') }}">
+    <link rel="mask-icon" href="{{ url_for('static', filename='img/mask.svg') }}" color="#56BE37">
+    <link rel="mask-icon-theme" href="{{ url_for('static', filename='img/mask-theme.svg') }}" color="#56BE37">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ url_for('static', filename='img/apple-touch-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ url_for('static', filename='img/apple-touch-icon-144x144.png') }}">
+
+    <!-- le CSS -->
+
+    <link rel="stylesheet" href="{{ url_for("static", filename="css/bootstrap.min.css") }}">
+    <link rel="stylesheet" href="{{ url_for("static", filename="css/bootstrap-responsive.min.css") }}">
+    <link rel="stylesheet" href="{{ url_for("static", filename="vendor/font-awesome-4.7.0/css/font-awesome.min.css") }}">
+    <link rel="stylesheet" href="{{ url_for("static", filename="forcelogin/css/forcelogin.css") }}">
+
+    {% for url in forcelogin_theming %}
+        <link rel="stylesheet" href="{{ url }}">
+    {% endfor %}
+
+    <!-- le javascript -->
+
+    <script>
+        var BASE_URL = "{{ url_for('index') }}";
+    </script>
+    <script src="{{ url_for("static", filename="js/lib/jquery/jquery.min.js") }}"></script>
+    <script src="{{ url_for("static", filename="js/lib/sockjs.min.js") }}"></script>
+    <script src="{{ url_for("static", filename="js/lib/bootstrap/bootstrap.js") }}"></script>
+    <script src="{{ url_for("static", filename="js/lib/lodash.min.js") }}"></script>
+
+    {% assets "js_client" %}
+        <script type="text/javascript" src="{{ ASSET_URL }}"></script>
+    {% endassets %}
+
+    <script src="{{ url_for("static", filename="forcelogin/js/forcelogin.js") }}"></script>
+</head>
+<body>
+    <div class="container">
+        <form class="form-signin">
+            <h2 class="form-signin-heading">{{ _('Please log in') }}</h2>
+
+            <div id="login-error" class="alert alert-error">{{ _('Incorrect username or password.') }}</div>
+            <div id="login-offline" class="alert alert-error">{{ _('Server is currently offline.') }} <a id="login-reconnect" href="javascript:void(0)">{{ _('Reconnect...') }}</a></div>
+
+            <input type="text" id="login-user" class="input-block-level" placeholder="{{ _('Username')|edq }}" autofocus>
+            <input type="password" id="login-password" class="input-block-level" placeholder="{{ _('Password')|edq }}">
+            <span class="pull-right"><small><a href="https://faq.octoprint.org/forgotten-password" id="login-forgotpassword" target="_blank" tabindex="-1">{{ _('Forgot password?') }}</a></small></span>
+            <label class="checkbox">
+                <input type="checkbox" id="login-remember"> {{ _('Remember me') }}
+            </label>
+            <button class="btn btn-block btn-large btn-primary" id="login-button" type="submit">{{ _('Log in') }}</button>
+        </form>
+    </div>
+
+    <div id="login-overlay">
+        <div class="background"></div>
+        <div class="wrapper">
+            <div class="outer">
+                <div class="inner">
+                    <div class="content">
+                        <i class="fa fa-spinner fa-spin fa-4x"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

src/octoprint/templates/index.jinja2
@@ -20,8 +20,7 @@
         <noscript>
             <style type="text/css">
                 #page-container-main,
-                #page-container-loading,
-                #page-container-needlogin {
+                #page-container-loading {
                     display:none;
                 }
             </style>
@@ -54,22 +53,6 @@
             </div>
         </div>
         {% endif %}
-
-        <div class="container" id="page-container-needlogin" style="display: none" data-bind="visible: needlogin">
-            <form class="form-signin">
-                <h2 class="form-signin-heading">{{ _('Please log in') }}</h2>
-
-                <div class="alert alert-error" style="display: none" data-bind="visible: login_error, text: login_error"></div>
-
-                <input type="text" data-bind="value: login_username" class="input-block-level" placeholder="{{ _('Username')|edq }}" autofocus>
-                <input type="password" data-bind="value: login_password" class="input-block-level" placeholder="{{ _('Password')|edq }}">
-                <span class="pull-right"><small><a href="https://faq.octoprint.org/forgotten-password" id="login-forgotpassword" target="_blank" tabindex="-1">{{ _('Forgot password?') }}</a></small></span>
-                <label class="checkbox">
-                    <input type="checkbox" data-bind="checked: login_remember"> {{ _('Remember me') }}
-                </label>
-                <button class="btn btn-block btn-large btn-primary" type="submit" data-bind="click: login">{{ _('Log in') }}</button>
-            </form>
-        </div>
 
         <div class="page-container" id="page-container-main" {% if enableLoadAnimation %}style="display: none"{% endif %} data-bind="visible: visible">
             <!-- ko allowBindings: false -->

commit aadcf650c70c2428e7ac8c23475046d9635299b3

🐛 Fix UI caching again
Was broken by 3f3cb556a703f15c60907956e66f8b0abdef6a5d in between server
restarts due to the wrong stuff being fetched by the preemptive cache.

src/octoprint/server/views.py
@@ -411,7 +411,7 @@ def index():
 		return util.flask.add_non_caching_response_headers(make_response(render_template("forcelogin.jinja2")))
 
 	def default_view():
-		if Permissions.STATUS.can() and Permissions.SETTINGS_READ.can():
+		if request.headers.get("X-Preemptive-Record", None) == "no" or (Permissions.STATUS.can() and Permissions.SETTINGS_READ.can()):
 			return ui_view()
 		else:
 			return login_view()