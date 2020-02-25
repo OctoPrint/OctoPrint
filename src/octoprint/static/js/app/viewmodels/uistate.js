2af589a823c542448af98420bba3bf1a40d98938
Gina Häußge <gina@octoprint.org>
2020-02-25 15:37:14 +0100

    ♻ Remove remnants of old login ui

--- src/octoprint/static/js/app/viewmodels/uistate.js
+++ src/octoprint/static/js/app/viewmodels/uistate.js
@@ -2,49 +2,11 @@ $(function() {
     function UiStateViewModel(parameters) {
         var self = this;
 
-        self.loginState = parameters[0];
-        self.access = parameters[1];
-
         self.loading = ko.observable(CONFIG_LOADINGANIMATION);
         self.loading_error = ko.observable(false);
-        self.needlogin = ko.computed(function() {
-            return !self.loading() && !self.loginState.hasAllPermissionsKo(self.access.permissions.STATUS, self.access.permissions.SETTINGS_READ)();
-        });
         self.visible = ko.pureComputed(function() {
-            return !self.loading() && !self.needlogin();
+            return !self.loading();
         });
-
-        self.login_username = ko.observable();
-        self.login_password = ko.observable();
-        self.login_remember = ko.observable(false);
-        self.login_error = ko.observable(false);
-
-        self.login = function() {
-            self.login_error(false);
-            self.loginState.login(self.login_username(), self.login_password(), self.login_remember(), false)
-                .done(function() {
-                    self.login_username(undefined);
-                    self.login_password(undefined);
-                })
-                .fail(function(response) {
-                    switch(response.status) {
-                        case 401: {
-                            self.login_error(gettext("User unknown or wrong password"));
-                            break;
-                        }
-                        case 403: {
-                            self.login_error(gettext("Your account is deactivated"));
-                            break;
-                        }
-                    }
-                });
-        };
-
-        self.onUserLoggedOut = self.onUserPermissionsChanged = function() {
-            if (!self.loginState.hasAllPermissions(self.access.permissions.STATUS, self.access.permissions.SETTINGS_READ) && !CONFIG_FIRST_RUN) {
-                location.reload();
-            }
-        };
 
         self.showLoadingError = function(error) {
             log.error("Loading error: " + error + " Please check prior messages and 'octoprint.log' for possible reasons.");
@@ -58,7 +20,6 @@ $(function() {
 
     OCTOPRINT_VIEWMODELS.push({
         construct: UiStateViewModel,
-        dependencies: ["loginStateViewModel", "accessViewModel"],
-        elements: ["#page-container-main", "#page-container-loading", "#page-container-needlogin"]
+        elements: ["#page-container-main", "#page-container-loading"]
     });
 });
