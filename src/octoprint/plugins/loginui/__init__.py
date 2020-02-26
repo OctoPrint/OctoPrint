054df68741e12190bdf7760f384e272d69c86dc4
Gina Häußge <gina@octoprint.org>
2020-02-26 09:13:44 +0100

    📝 Fix year

--- src/octoprint/plugins/loginui/__init__.py
+++ src/octoprint/plugins/loginui/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
-__copyright__ = "Copyright (C) 2018 The OctoPrint Project - Released under terms of the AGPLv3 License"
+__copyright__ = "Copyright (C) 2020 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
 import octoprint.plugin
 
