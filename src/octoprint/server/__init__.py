6b379fbef333d9815c0f60ec18ef6805b63cb1f3
Gina Häußge <gina@octoprint.org>
2020-04-15 14:12:23 +0200

    ✨ Make rjsmin use configurable again

--- src/octoprint/server/__init__.py
+++ src/octoprint/server/__init__.py
@@ -1588,8 +1588,12 @@ class Server(object):
 
 		# -- JS --------------------------------------------------------------------------------------------------------
 
-		js_filters = ["sourcemap_remove", "js_delimiter_bundler", "rjsmin", "gzip"]
-		js_plugin_filters = ["sourcemap_remove", "js_delimiter_bundler", "rjsmin", "gzip"]
+		filters = ["sourcemap_remove", "js_delimiter_bundler"]
+		if self._settings.getBoolean(["devel", "webassets", "minify"]):
+			filters.append("rjsmin")
+		filters.append("gzip")
+
+		js_filters = js_plugin_filters = filters
 
 		def js_bundles_for_plugins(collection, filters=None):
 			"""Produces JsPluginBundle instances that output IIFE wrapped assets"""
