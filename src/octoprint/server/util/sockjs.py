OctoPrint/OctoPrint

commit 9e3cf58bc9941540f16a2fa55a8c666499a3ad55

🐛 Better session validation
Removed side effects for login behaviour after server restart

src/octoprint/server/util/sockjs.py
@@ -204,9 +204,9 @@ class PrinterStateConnection(octoprint.vendor.sockjs.tornado.SockJSConnection,
 				self._logger.warning("Got invalid auth message from client {}, ignoring: {!r}".format(self._remoteAddress, message["auth"]))
 			else:
 				user_id, user_session = parts
-				user = self._userManager.find_user(userid=user_id, session=user_session)
 
-				if user is not None:
+				if self._userManager.validate_user_session(user_id, user_session):
+					user = self._userManager.find_user(userid=user_id)
 					self._on_login(user)
 				else:
 					self._logger.warn("Unknown user/session combo: {}:{}".format(user_id, user_session))

src/octoprint/access/users.py
@@ -240,6 +240,13 @@ class UserManager(GroupChangeListener, object):
 					del self._session_users_by_session[session]
 			del self._sessionids_by_userid[username]
 
+	def validate_user_session(self, userid, session):
+		if session in self._session_users_by_session:
+			user = self._session_users_by_session[session]
+			return userid == user.get_id()
+
+		return False
+
 	def find_user(self, userid=None, session=None):
 		if session is not None and session in self._session_users_by_session:
 			user = self._session_users_by_session[session]
@@ -787,7 +794,7 @@ class FilebasedUserManager(UserManager):
 	def find_user(self, userid=None, apikey=None, session=None):
 		user = UserManager.find_user(self, userid=userid, session=session)
 
-		if user is not None or session is not None:
+		if user is not None:
 			return user
 
 		if userid is not None:

commit 1f5391fc2c7973b13c20c43466b5f8be6dc26f7a

Fix issue introduced by 9e3cf58bc9 which prevents _api user from connecting to SockJS.

src/octoprint/server/util/sockjs.py
@@ -206,7 +206,7 @@ class PrinterStateConnection(octoprint.vendor.sockjs.tornado.SockJSConnection,
 				user_id, user_session = parts
 
 				if self._userManager.validate_user_session(user_id, user_session):
-					user = self._userManager.find_user(userid=user_id)
+					user = self._userManager.find_user(userid=user_id, session=user_session)
 					self._on_login(user)
 				else:
 					self._logger.warn("Unknown user/session combo: {}:{}".format(user_id, user_session))