def106f409aa8f46f94d149bac1599d0e538325e
Gina Häußge <gina@octoprint.org>
2020-06-18 14:12:59 +0200

    🐛 Fix a JS error due to misnaming

--- src/octoprint/static/js/app/viewmodels/settings.js
+++ src/octoprint/static/js/app/viewmodels/settings.js
@@ -283,9 +283,9 @@ $(function() {
         self.server_onlineCheckResolutionOk = ko.observable(false);
         self.server_onlineCheckResolutionBroken = ko.observable(false);
         self.server_onlineCheckResolutionReset = function() {
-            self.server_onlineResolutionCheckText("");
-            self.server_onlineResolutionCheckOk(false);
-            self.server_onlineResolutionCheckBroken(false);
+            self.server_onlineCheckResolutionText("");
+            self.server_onlineCheckResolutionOk(false);
+            self.server_onlineCheckResolutionBroken(false);
         };
 
         var folderTypes = ["uploads", "timelapse", "timelapseTmp", "logs", "watched"];
