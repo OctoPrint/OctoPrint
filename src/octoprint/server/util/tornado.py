5287dca4e5a063bd99a0156b63bdd09756611c50
Gina Häußge <gina@octoprint.org>
2020-06-26 10:16:50 +0200

    🐛 Flask login changed from user_id to _user_id
    Thanks to @bzed for the heads-up.

--- src/octoprint/server/util/tornado.py
+++ src/octoprint/server/util/tornado.py
@@ -1284,7 +1284,7 @@ def access_validation_factory(app, validator, *args):
 		wsgi_environ = WsgiInputContainer.environ(request)
 		with app.request_context(wsgi_environ):
 			session = app.session_interface.open_session(app, flask.request)
-			user_id = session.get("user_id")
+			user_id = session.get("_user_id")
 			user = None
 
 			# Yes, using protected methods is ugly. But these used to be publicly available in former versions
