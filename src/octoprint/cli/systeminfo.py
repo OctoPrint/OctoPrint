55291b6bfdf63151df54918e7f2b448d0905d705
Gina Häußge <gina@octoprint.org>
2021-01-29 15:37:24 +0100

    ✨ Add cli to generate system info bundle
    To print the system info:
    
        octoprint systeminfo
    
    To export a system info bundle:
    
        octoprint systeminfo <targetpath>
    
    e.g.
    
        octoprint systeminfo .

--- src/octoprint/cli/systeminfo.py
+++ src/octoprint/cli/systeminfo.py
@@ -6,17 +6,22 @@ __copyright__ = "Copyright (C) 2015 The OctoPrint Project - Released under terms
 
 
 import logging
+import os
 
 import click
+import zipstream
 
 from octoprint.cli import init_platform_for_cli, standard_options
 
 click.disable_unicode_literals_warning = True
 
 
-def get_systeminfo(environment_detector, connectivity_checker):
+def get_systeminfo(environment_detector, connectivity_checker, additional_fields=None):
     from octoprint import __version__
     from octoprint.util import dict_flatten
+
+    if additional_fields is None:
+        additional_fields = {}
 
     environment_detector.run_detection(notify_plugins=False)
 
@@ -30,7 +35,68 @@ def get_systeminfo(environment_detector, connectivity_checker):
     flattened = dict_flatten(systeminfo)
     flattened["env.python.virtualenv"] = "env.python.virtualenv" in flattened
 
+    for k, v in additional_fields.items():
+        if k not in flattened:
+            flattened[k] = v
+
     return flattened
+
+
+def get_systeminfo_bundle(systeminfo, logbase, printer=None):
+    from octoprint.util import to_bytes
+
+    systeminfotxt = []
+    for k in sorted(systeminfo.keys()):
+        systeminfotxt.append("{}: {}".format(k, systeminfo[k]))
+
+    terminaltxt = None
+    if printer and printer.is_operational():
+        firmware_info = printer.firmware_info
+        if firmware_info:
+            systeminfo["printer.firmware"] = firmware_info["name"]
+
+        if hasattr(printer, "_log"):
+            terminaltxt = list(printer._log)
+
+    try:
+        import zlib  # noqa: F401
+
+        compress_type = zipstream.ZIP_DEFLATED
+    except ImportError:
+        # no zlib, no compression
+        compress_type = zipstream.ZIP_STORED
+
+    z = zipstream.ZipFile()
+
+    # add systeminfo
+    z.writestr(
+        "systeminfo.txt", to_bytes("\n".join(systeminfotxt)), compress_type=compress_type
+    )
+
+    # add terminal.txt, if available
+    if terminaltxt:
+        z.writestr(
+            "terminal.txt", to_bytes("\n".join(terminaltxt)), compress_type=compress_type
+        )
+
+    # add logs
+    for log in (
+        "octoprint.log",
+        "serial.log",
+        "plugin_softwareupdate_console.log",
+        "plugin_pluginmanager_console.log",
+    ):
+        logpath = os.path.join(logbase, log)
+        if os.path.exists(logpath):
+            z.write(logpath, arcname=log, compress_type=compress_type)
+
+    return z
+
+
+def get_systeminfo_bundle_name():
+    import time
+
+    return "octoprint-systeminfo-{}.zip".format(time.strftime("%Y%m%d%H%M%S"))
 
 
 @click.group()
@@ -40,8 +106,14 @@ def systeminfo_commands():
 
 @systeminfo_commands.command(name="systeminfo")
 @standard_options()
+@click.argument(
+    "path",
+    nargs=1,
+    required=False,
+    type=click.Path(writable=True, dir_okay=True, resolve_path=True),
+)
 @click.pass_context
-def systeminfo_command(ctx, **kwargs):
+def systeminfo_command(ctx, path, **kwargs):
     """Retrieves and prints the system info."""
     logging.disable(logging.ERROR)
     try:
@@ -59,8 +131,32 @@ def systeminfo_command(ctx, **kwargs):
         click.echo("There was a fatal error initializing the platform.", err=True)
         ctx.exit(-1)
     else:
-        systeminfo = get_systeminfo(environment_detector, connectivity_checker)
+        systeminfo = get_systeminfo(
+            environment_detector, connectivity_checker, {"systeminfo.generator": "cli"}
+        )
 
-        for k in sorted(systeminfo.keys()):
-            click.echo("{}: {}".format(k, systeminfo[k]))
+        if path:
+            # create zip at path
+            zipfilename = os.path.join(path, get_systeminfo_bundle_name())
+            click.echo("Writing systeminfo bundle to {}...".format(zipfilename))
+
+            z = get_systeminfo_bundle(systeminfo, settings.getBaseFolder("logs"))
+            try:
+                with open(zipfilename, "wb") as f:
+                    for data in z:
+                        f.write(data)
+            except Exception as e:
+                click.echo(str(e), err=True)
+                click.echo(
+                    "There was an error writing to {}.".format(zipfilename), err=True
+                )
+                ctx.exit(-1)
+
+            click.echo("Done!")
+            click.echo(zipfilename)
+
+        else:
+            # output systeminfo to console
+            for k in sorted(systeminfo.keys()):
+                click.echo("{}: {}".format(k, systeminfo[k]))
     ctx.exit(0)
