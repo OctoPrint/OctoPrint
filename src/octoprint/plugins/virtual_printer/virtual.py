d6e1868569e454619db3adf895e860704c19c60f
k <k@drax>
2021-02-28 12:29:37 -0600

    minor fix for M117 with no args

--- src/octoprint/plugins/virtual_printer/virtual.py
+++ src/octoprint/plugins/virtual_printer/virtual.py
@@ -745,7 +745,10 @@ class VirtualPrinter(object):
         # type: (str) -> None
         # we'll just use this to echo a message, to allow playing around with pause triggers
         if self._echoOnM117:
-            self._send("echo:%s" % re.search(r"M117\s+(.*)", data).group(1))
+            try:
+                self._send("echo:%s" % re.search(r"M117\s+(.*)", data).group(1))
+            except AttributeError:
+                self._send("echo:")
 
     def _gcode_M155(self, data):
         # type: (str) -> None
