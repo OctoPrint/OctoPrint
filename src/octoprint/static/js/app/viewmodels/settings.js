f8c529723bd85c983165a9a683653b015b06f808
John Boiles <johnaboiles@gmail.com>
2021-09-14 19:58:49 +0100

    Add controls to allow for PIP and fullscreen

--- src/octoprint/static/js/app/viewmodels/settings.js
+++ src/octoprint/static/js/app/viewmodels/settings.js
@@ -456,7 +456,7 @@ $(function () {
                 }
             } else if (isWebRTCAvailable() && streamType == "webrtc") {
                 webcam_element = $(
-                    '<video id="webcam_webrtc" muted autoplay playsinline style="width: 100%"/>'
+                    '<video id="webcam_webrtc" muted autoplay playsinline controls style="width: 100%"/>'
                 );
                 video_element = webcam_element[0];
 
