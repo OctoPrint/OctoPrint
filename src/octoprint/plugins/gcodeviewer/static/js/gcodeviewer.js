4f7db5d941de3c8c581255ca19fa933c117ddedb
Quinn Damerell <quinnd@outlook.com>
2021-10-22 09:47:12 -0700

    Fixing code formatting

--- src/octoprint/plugins/gcodeviewer/static/js/gcodeviewer.js
+++ src/octoprint/plugins/gcodeviewer/static/js/gcodeviewer.js
@@ -476,16 +476,16 @@ $(function () {
                 .on("slide", self.changeCommandRange);
         };
 
-        self._configureContainerElement = function(containerElement) {
+        self._configureContainerElement = function (containerElement) {
             // Prevent the default browser action for the mouse wheel down event. The desired behavor is to have the
             // gocde canvas pan on mouse down + drag, which happens. But if we don't prevent the default action, the browser
             // will also scroll the entire page.
-            containerElement.mousedown(function(event){
+            containerElement.mousedown(function (event) {
                 // Middle mouse button
                 if (event.which === 2) {
                     event.preventDefault();
                 }
-            })
+            });
         };
 
         self.loadFile = function (path, date) {
