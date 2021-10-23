1baf7b2c666fa91ba61de9308a801b0e61238749
Maciej Urbanski <rooter@kyberian.net>
2021-10-24 01:59:41 +0200

    test against python 3.10

--- setup.py
+++ setup.py
@@ -230,6 +230,8 @@ def params():
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: JavaScript",
         "Topic :: Printing",
