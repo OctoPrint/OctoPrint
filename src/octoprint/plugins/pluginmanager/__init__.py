78deafd9b9de142006878cd261e6b2599f49120b
Rotzbua <Rotzbua@users.noreply.github.com>
2021-11-15 17:58:58 +0100

    Change links to https for docs.octoprint.org

--- src/octoprint/plugins/pluginmanager/__init__.py
+++ src/octoprint/plugins/pluginmanager/__init__.py
@@ -2155,7 +2155,7 @@ def _register_custom_events(*args, **kwargs):
 
 __plugin_name__ = "Plugin Manager"
 __plugin_author__ = "Gina Häußge"
-__plugin_url__ = "http://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html"
+__plugin_url__ = "https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html"
 __plugin_description__ = "Allows installing and managing OctoPrint plugins"
 __plugin_license__ = "AGPLv3"
 __plugin_pythoncompat__ = ">=2.7,<4"
