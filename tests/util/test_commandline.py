94a84a0c7d49d85e5f1a766e3b57eb6e821c918b
Gina Häußge <gina@octoprint.org>
2022-02-07 14:31:34 +0100

    🗑️ clean_ansi will no longer accept bytes in the future
    Also add some type annotations and a deprecation warning
    to clarify that. To be removed with 2.0.0.

---
+++ tests/util/test_commandline.py
@@ -0,0 +1,34 @@
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2022 The OctoPrint Project - Released under terms of the AGPLv3 License"
+
+import unittest
+
+import ddt
+import pytest
+
+import octoprint.util.commandline
+
+
+@ddt.ddt
+class CommandlineTest(unittest.TestCase):
+    @ddt.data(
+        (
+            "Some text with some \x1b[31mred words\x1b[39m in it",
+            "Some text with some red words in it",
+        ),
+        (
+            "We \x1b[?25lhide the cursor here and then \x1b[?25hshow it again here",
+            "We hide the cursor here and then show it again here",
+        ),
+    )
+    @ddt.unpack
+    def test_clean_ansi(self, input, expected):
+        actual = octoprint.util.commandline.clean_ansi(input)
+        self.assertEqual(expected, actual)
+
+    def test_clean_ansi_deprecated(self):
+        with pytest.deprecated_call():
+            actual = octoprint.util.commandline.clean_ansi(
+                b"Some bytes with some \x1b[31mred words\x1b[39m in it"
+            )
+            self.assertEqual(b"Some bytes with some red words in it", actual)
