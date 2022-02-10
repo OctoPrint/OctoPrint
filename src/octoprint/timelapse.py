6430b239b2ad4e226ef5644066d9099c72093c91
jneilliii <jneilliii+github@gmail.com>
2022-02-10 12:30:20 -0500

    🐛 Fix timelapse snapshots for files with % in the name (#4366)
    Fixes #4365

--- src/octoprint/timelapse.py
+++ src/octoprint/timelapse.py
@@ -604,7 +604,8 @@ class Timelapse:
         self._in_timelapse = True
         self._gcode_file = os.path.basename(gcode_file)
         self._file_prefix = "{}_{}".format(
-            os.path.splitext(self._gcode_file)[0], time.strftime("%Y%m%d%H%M%S")
+            os.path.splitext(self._gcode_file)[0].replace("%", "%%"),
+            time.strftime("%Y%m%d%H%M%S"),
         )
 
     def stop_timelapse(self, do_create_movie=True, success=True):
