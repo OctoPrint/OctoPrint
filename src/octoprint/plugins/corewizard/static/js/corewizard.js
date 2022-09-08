15a05c9ec9154d76e8f8df015372f5922e24a0ad
Frederik Kemner <stuff+github@fredo.org>
2022-09-08 14:59:51 +0200

    fix error state in core wizard
    separately handle invalid user name

--- src/octoprint/plugins/corewizard/static/js/corewizard.js
+++ src/octoprint/plugins/corewizard/static/js/corewizard.js
@@ -12,6 +12,14 @@ $(function () {
 
         self.required = false;
 
+        self.invalidUsername = ko.pureComputed(function () {
+            return (
+                self.username() &&
+                self.username().trim() !== "" &&
+                self.username() !== self.username().trim()
+            );
+        });
+
         self.passwordMismatch = ko.pureComputed(function () {
             return self.password() !== self.confirmedPassword();
         });
@@ -30,7 +38,10 @@ $(function () {
 
         self.validData = ko.pureComputed(function () {
             return (
-                !self.passwordMismatch() && self.validUsername() && self.validPassword()
+                !self.invalidUsername() &&
+                !self.passwordMismatch() &&
+                self.validUsername() &&
+                self.validPassword()
             );
         });
 
