f26d5f01ef85a18a32cfe69ed6aee5f5f0d0a7cb
Christian Würthner <c.wurthner@gmail.com>
2022-11-12 15:13:59 +0100

    Add file header

--- src/octoprint/webcams.py
+++ src/octoprint/webcams.py
@@ -1,3 +1,7 @@
+__author__ = "Gina Häußge <osd@foosel.net>"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2022 The OctoPrint Project - Released under terms of the AGPLv3 License"
+
 import logging
 
 import octoprint.plugin
