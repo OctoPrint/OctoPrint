7563fe332ac2beab5318076689bded7c76aca217
Gina Häußge <gina@octoprint.org>
2024-02-15 17:26:06 +0100

    🐛 Keep config hash in sync after connect
    We so far only ever set it on server connect, which would cause
    a reload overlay to trigger on every reconnect if the settings
    had been changed in any way since the initial connect.
    
    This should prevent this in most cases.

--- src/octoprint/static/js/app/dataupdater.js
+++ src/octoprint/static/js/app/dataupdater.js
@@ -469,6 +469,8 @@ function DataUpdater(allViewModels, connectCallback, disconnectCallback) {
                 });
             } else if (type === "ConnectivityChanged") {
                 ONLINE = payload.new;
+            } else if (type === "SettingsUpdated") {
+                self.config_hash = payload.config_hash;
             }
 
             var legacyEventHandlers = {
