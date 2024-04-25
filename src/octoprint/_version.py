cc607d50b0f06ff2f9b79c8177dd4b6fcaa70c63
Gina Häußge <gina@octoprint.org>
2024-04-25 14:28:10 +0200

    🐛 Fix staging/bugfix version

--- src/octoprint/_version.py
+++ src/octoprint/_version.py
@@ -52,8 +52,8 @@ improve/.* 1.10.0 cd955e9a46782119b36cc22b8dea5652ebbf9774
 
 # staging/bugfix is the branch for preparation of the 1.10.x bugfix releases
 # so are any bug/... branches
-staging/bugfix 1.10.1 fed2dd7660c1587e2c280a30ed9d84ceffa1b89d pep440-dev
-bug/.* 1.10.1 fed2dd7660c1587e2c280a30ed9d84ceffa1b89d pep440-dev
+staging/bugfix 1.10.1 fed2dd7660c1587e2c280a30ed9d84ceffa1b89d
+bug/.* 1.10.1 fed2dd7660c1587e2c280a30ed9d84ceffa1b89d
 
 # staging/maintenance is currently the branch for preparation of 1.10.0rc5
 # so is regressionfix/...
