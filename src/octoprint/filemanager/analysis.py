7851ef5f7b49d6b31739580b88dfdc855638df39
Gina Häußge <gina@octoprint.org>
2024-05-13 11:36:37 +0200

    Revert ":bug: GCODE analysis: fix running against files with whitespace (#5009)"
    This reverts commit 25bf7f8313f168554d10fd3e4140052eeb74a11d.

--- src/octoprint/filemanager/analysis.py
+++ src/octoprint/filemanager/analysis.py
@@ -481,7 +481,7 @@ class GcodeAnalysisQueue(AbstractAnalysisQueue):
                 command += ["--offset", str(offset[0]), str(offset[1])]
             if g90_extruder:
                 command += ["--g90-extruder"]
-            command.append(f'"{self._current.absolute_path}"')
+            command.append(self._current.absolute_path)
 
             self._logger.info(f"Invoking analysis command: {' '.join(command)}")
 
