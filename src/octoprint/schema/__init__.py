2838f87071308097e8d7c3d1781c9aa11033efc3
Gina Häußge <gina@octoprint.org>
2024-06-12 18:09:16 +0200

    ♻️ Implement fallback for Python 3.7
    Pydantic 2 doesn't support this any longer, so use pydantic 1 for that
    and implement `model_dump` on our side.

--- src/octoprint/schema/__init__.py
+++ src/octoprint/schema/__init__.py
@@ -1,9 +1,33 @@
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = "Copyright (C) 2022 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
+
 from pydantic import BaseModel as PydanticBaseModel
 
+try:
+    # pydantic 2.x
+    import pydantic.v1 as pydantic_v1  # noqa: F401
 
-class BaseModel(PydanticBaseModel):
-    class Config:
-        use_enum_values = True
+    del pydantic_v1
+
+    from pydantic import ConfigDict
+
+    class BaseModel(PydanticBaseModel):
+        model_config = ConfigDict(use_enum_values=True)
+
+except ImportError:
+    # pydantic 1.x
+
+    class BaseModel(PydanticBaseModel):
+        class Config:
+            use_enum_values = True
+
+        def model_dump(self, *args, **kwargs):
+            # not supported in pydantic 1.x
+            kwargs.pop("mode")
+            kwargs.pop("context")
+            kwargs.pop("round_trip")
+            kwargs.pop("warnings")
+            kwargs.pop("serialize_as_any")
+
+            return self.dict(*args, **kwargs)
--- src/octoprint/vendor/with_attrs_docs.py
+++ src/octoprint/vendor/with_attrs_docs.py
@@ -7,26 +7,53 @@ from typing import Type
 from class_doc import extract_docs_from_cls_obj
 from pydantic import BaseModel
 
+try:
+    # pydantic 2.x
+    import pydantic.v1 as pydantic_v1
+    del pydantic_v1
 
-def apply_attributes_docs(
-    model: Type[BaseModel], override_existing: bool = True
-) -> None:
-    """
-    Apply model attributes documentation in-place. Resulted docs are placed
-    inside :code:`field.schema.description` for *pydantic* model field.
-    :param model: any pydantic model
-    :param override_existing: override existing descriptions
-    """
-    docs = extract_docs_from_cls_obj(model)
+    def apply_attributes_docs(
+        model: Type[BaseModel], override_existing: bool = True
+    ) -> None:
+        """
+        Apply model attributes documentation in-place. Resulted docs are placed
+        inside :code:`field.schema.description` for *pydantic* model field.
+        :param model: any pydantic model
+        :param override_existing: override existing descriptions
+        """
+        docs = extract_docs_from_cls_obj(model)
 
-    for field in model.__fields__.values():
-        if field.field_info.description and not override_existing:
-            continue
+        for name, field in model.model_fields.items():
+            if field.description and not override_existing:
+                continue
 
-        try:
-            field.field_info.description = '\n'.join(docs[field.name])
-        except KeyError:
-            pass
+            try:
+                field.description = '\n'.join(docs[name])
+            except KeyError:
+                pass
+
+
+except ImportError:
+    # pydantic 1.x
+    def apply_attributes_docs(
+        model: Type[BaseModel], override_existing: bool = True
+    ) -> None:
+        """
+        Apply model attributes documentation in-place. Resulted docs are placed
+        inside :code:`field.schema.description` for *pydantic* model field.
+        :param model: any pydantic model
+        :param override_existing: override existing descriptions
+        """
+        docs = extract_docs_from_cls_obj(model)
+
+        for field in model.__fields__.values():
+            if field.field_info.description and not override_existing:
+                continue
+
+            try:
+                field.field_info.description = '\n'.join(docs[field.name])
+            except KeyError:
+                pass
 
 
 def with_attrs_docs(
