2b63a43ab23cbc706369329fe7418a7409821ec3
Nathan Schulte <nmschulte@desmas.net>
2024-07-21 13:08:11 -0500

    GCode Viewer: correct Web Workers API check
    the custom Modernizr build no longer checks for Web Workers API, so perform the check manually

--- src/octoprint/plugins/gcodeviewer/static/js/viewer/ui.js
+++ src/octoprint/plugins/gcodeviewer/static/js/viewer/ui.js
@@ -98,7 +98,7 @@ GCODE.ui = (function () {
 
         var errorList = document.getElementById("errorList");
 
-        if (!Modernizr.webworkers) {
+        if (!window.Worker) {
             fatal.push(
                 "<li>Your browser doesn't seem to support HTML5 Web Workers, this application won't work without it.</li>"
             );
