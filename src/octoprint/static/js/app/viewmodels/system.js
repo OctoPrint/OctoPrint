495006e5a024e91f663069ae5d590a395c129549
Gina Häußge <gina@octoprint.org>
2024-08-27 12:56:29 +0200

    fix[ux]: Display python eol popup only once per 30d
    Uses a cookie with a 30d max-age to ensure that the popup doesn't get
    shown on every UI reload.
    
    The cookie value is set to the combination of python version and current
    EOL date, so if that changes, the popup will be shown again.
    
    A cookie value of "true" will always hide the popup, this is useful for E2E
    testing where we don't want the popup from interfering with our automated
    clicks.

--- src/octoprint/static/js/app/viewmodels/system.js
+++ src/octoprint/static/js/app/viewmodels/system.js
@@ -103,55 +103,82 @@ $(function () {
             }
 
             if (startupData.python_eol) {
-                if (self._pythonEOLNotification) self._pythonEOLNotification.remove();
+                const COOKIE_NAME = "python_eol_notified";
+                const COOKIE_VALUE = `${startupData.python_eol.version};${startupData.python_eol.date}`;
 
-                let eolStatement;
-                if (startupData.python_eol.soon) {
-                    eolStatement = gettext(
-                        "Your Python version %(python)s is nearing its end of life (%(date)s)."
-                    );
-                } else {
-                    eolStatement = gettext(
-                        "Your Python version %(python)s is past its end of life (%(date)s)."
-                    );
+                if (self._pythonEOLNotification) {
+                    self._pythonEOLNotification.remove();
+                    self._pythonEOLNotification = undefined;
                 }
 
-                if (startupData.python_eol.last_octoprint) {
-                    octoprintStatement = _.sprintf(
+                const currentCookieValue = OctoPrint.getCookie(COOKIE_NAME);
+                if (
+                    currentCookieValue !== "true" &&
+                    currentCookieValue !== COOKIE_VALUE
+                ) {
+                    let eolStatement;
+                    if (startupData.python_eol.soon) {
+                        eolStatement = gettext(
+                            "Your Python version %(python)s is nearing its end of life (%(date)s)."
+                        );
+                    } else {
+                        eolStatement = gettext(
+                            "Your Python version %(python)s is past its end of life (%(date)s)."
+                        );
+                    }
+
+                    if (startupData.python_eol.last_octoprint) {
+                        octoprintStatement = _.sprintf(
+                            gettext(
+                                "OctoPrint %(octoprint)s will be the last version to support this Python version."
+                            ),
+                            {octoprint: _.escape(startupData.python_eol.last_octoprint)}
+                        );
+                    } else {
+                        octoprintStatement = gettext(
+                            "A future version of OctoPrint will drop support for this Python version."
+                        );
+                    }
+
+                    const html =
+                        "<p>" +
+                        _.sprintf(eolStatement, {
+                            python: _.escape(startupData.python_eol.version),
+                            date: _.escape(startupData.python_eol.date)
+                        }) +
+                        " " +
+                        octoprintStatement +
+                        " " +
+                        gettext("You should upgrade as soon as possible!") +
+                        "</p>" +
+                        "<p>" +
                         gettext(
-                            "OctoPrint %(octoprint)s will be the last version to support this Python version."
-                        ),
-                        {octoprint: _.escape(startupData.python_eol.last_octoprint)}
+                            "Please refer to the FAQ for recommended upgrade workflows:"
+                        ) +
+                        "</p>" +
+                        "<p><a href='https://faq.octoprint.org/python-upgrade' target='_blank' rel='noopener noreferer'>How to migrate to another Python version</a></p>" +
+                        "<p><small>" +
+                        gettext(
+                            "This will be shown again every 30 days until you have upgraded your Python."
+                        ) +
+                        "</small></p>";
+
+                    self._pythonEOLNotification = new PNotify({
+                        title: gettext("Warning"),
+                        text: html,
+                        hide: false
+                    });
+                    self._pythonEOLNotification.elem.attr(
+                        "data-test-id",
+                        "notification-python-eol"
                     );
-                } else {
-                    octoprintStatement = gettext(
-                        "A future version of OctoPrint will drop support for this Python version."
+
+                    OctoPrint.setCookie(
+                        COOKIE_NAME,
+                        COOKIE_VALUE,
+                        {maxage: 30 * 24 * 60 * 60} // 30d
                     );
                 }
-
-                const html =
-                    "<p>" +
-                    _.sprintf(eolStatement, {
-                        python: _.escape(PYTHON_VERSION),
-                        date: _.escape(startupData.python_eol.date)
-                    }) +
-                    " " +
-                    octoprintStatement +
-                    " " +
-                    gettext("You should upgrade as soon as possible!") +
-                    "</p>" +
-                    "<p>" +
-                    gettext(
-                        "Please refer to the FAQ for recommended upgrade workflows:"
-                    ) +
-                    "</p>" +
-                    "<p><a href='https://faq.octoprint.org/python-upgrade' target='_blank' rel='noopener noreferer'>How to migrate to another Python version</a></p>";
-
-                self._pythonEOLNotification = new PNotify({
-                    title: gettext("Warning"),
-                    text: html,
-                    hide: false
-                });
             }
         };
 
