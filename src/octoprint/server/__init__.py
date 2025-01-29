OctoPrint/OctoPrint

commit 3a61906d750e0ba82371ee43de46c41c27811277

🔥 Clean up some left overs in authenticating
Closes #4968

src/octoprint/server/__init__.py
@@ -143,7 +143,6 @@ from octoprint.server.util import (
     corsRequestHandler,
     corsResponseHandler,
     csrfRequestHandler,
-    loginFromApiKeyRequestHandler,
     requireLoginRequestHandler,
 )
 from octoprint.server.util.flask import PreemptiveCache, validate_session_signature
@@ -235,19 +234,26 @@ def load_user(id):
 
 
 def load_user_from_request(request):
-    user = None
+    # API key?
+    apikey = util.get_api_key(request)
+    if apikey:
+        user = util.get_user_for_apikey(apikey)
+        if user:
+            return user
 
     if settings().getBoolean(["accessControl", "trustBasicAuthentication"]):
         # Basic Authentication?
-        user = util.get_user_for_authorization_header(
-            request.headers.get("Authorization")
-        )
+        user = util.get_user_for_authorization_header(request)
+        if user:
+            return user
 
     if settings().getBoolean(["accessControl", "trustRemoteUser"]):
         # Remote user header?
         user = util.get_user_for_remote_user_header(request)
+        if user:
+            return user
 
-    return user
+    return None
 
 
 def unauthorized_user():
@@ -2038,7 +2044,6 @@ class Server:
             return
 
         blueprint.before_request(corsRequestHandler)
-        blueprint.before_request(loginFromApiKeyRequestHandler)
         blueprint.after_request(corsResponseHandler)
 
         if plugin.is_blueprint_csrf_protected():

src/octoprint/server/api/__init__.py
@@ -32,8 +32,6 @@ from octoprint.server.util import (
     corsRequestHandler,
     corsResponseHandler,
     csrfRequestHandler,
-    loginFromApiKeyRequestHandler,
-    loginFromAuthorizationHeaderRequestHandler,
     noCachingExceptGetResponseHandler,
 )
 from octoprint.server.util.flask import (
@@ -70,8 +68,6 @@ VERSION = "0.1"
 api.after_request(noCachingExceptGetResponseHandler)
 
 api.before_request(corsRequestHandler)
-api.before_request(loginFromAuthorizationHeaderRequestHandler)
-api.before_request(loginFromApiKeyRequestHandler)
 api.before_request(csrfRequestHandler)
 api.after_request(corsResponseHandler)
 

src/octoprint/server/util/__init__.py
@@ -6,6 +6,7 @@ import base64
 import datetime
 import logging
 import sys
+from typing import Optional, Union
 
 PY3 = sys.version_info >= (3, 0)  # should now always be True, kept for plugins
 
@@ -14,10 +15,9 @@ import flask_login
 
 import octoprint.server
 import octoprint.timelapse
-import octoprint.vendor.flask_principal as flask_principal
 from octoprint.plugin import plugin_manager
 from octoprint.settings import settings
-from octoprint.util import deprecated, to_unicode
+from octoprint.util import to_unicode
 
 from . import flask, sockjs, tornado, watchdog  # noqa: F401
 
@@ -51,86 +51,6 @@ class LoginMechanism:
         elif login_mechanism == cls.REMOTE_USER:
             return "Remote User header"
         return "unknown method"
-
-
-@deprecated(
-    "API keys are no longer needed for anonymous access and thus this is now obsolete"
-)
-def enforceApiKeyRequestHandler():
-    pass
-
-
-apiKeyRequestHandler = deprecated(
-    "apiKeyRequestHandler has been renamed to enforceApiKeyRequestHandler"
-)(enforceApiKeyRequestHandler)
-
-
-def loginFromApiKeyRequestHandler():
-    """
-    ``before_request`` handler for blueprints which creates a login session for the provided api key (if available)
-
-    App session keys are handled as anonymous keys here and ignored.
-
-    TODO 1.11.0: Do we still need this with load_user_from_request in place?
-    """
-    try:
-        if loginUserFromApiKey():
-            _flask.g.login_via_apikey = True
-    except InvalidApiKeyException:
-        _flask.abort(403, "Invalid API key")
-
-
-def loginFromAuthorizationHeaderRequestHandler():
-    """
-    ``before_request`` handler for creating login sessions based on the Authorization header.
-
-    TODO 1.11.0: Do we still need this with load_user_from_request in place?
-    """
-    try:
-        if loginUserFromAuthorizationHeader():
-            _flask.g.login_via_header = True
-    except InvalidApiKeyException:
-        _flask.abort(403, "Invalid credentials in Basic Authorization header")
-
-
-class InvalidApiKeyException(Exception):
-    pass
-
-
-def loginUserFromApiKey():
-    apikey = get_api_key(_flask.request)
-    if not apikey:
-        return False
-
-    user = get_user_for_apikey(apikey)
-    if user is None:
-        # invalid API key = no API key
-        raise InvalidApiKeyException("Invalid API key")
-
-    return _loginHelper(user, login_mechanism=LoginMechanism.APIKEY)
-
-
-def loginUserFromAuthorizationHeader():
-    authorization_header = get_authorization_header(_flask.request)
-    user = get_user_for_authorization_header(authorization_header)
-    return _loginHelper(user, login_mechanism=LoginMechanism.AUTHHEADER)
-
-
-def _loginHelper(user, login_mechanism=None):
-    if (
-        user is not None
-        and user.is_active
-        and flask_login.login_user(user, remember=False)
-    ):
-        flask_principal.identity_changed.send(
-            _flask.current_app._get_current_object(),
-            identity=flask_principal.Identity(user.get_id()),
-        )
-        if login_mechanism:
-            _flask.session["login_mechanism"] = login_mechanism
-            _flask.session["credentials_seen"] = False
-        return True
-    return False
 
 
 def requireLoginRequestHandler():
@@ -246,7 +166,25 @@ def optionsAllowOrigin(request):
     return resp
 
 
-def get_user_for_apikey(apikey):
+def get_user_for_apikey(apikey: str) -> "Optional[octoprint.access.users.User]":
+    """
+    Tries to find a user based on the given API key.
+
+    Will only perform any action if the API key is not None and not empty.
+
+    If the API key is the master key, the master user will be returned.
+
+    If the API key is a user key, the user will be returned.
+
+    If the API key is neither, the key will be passed to all registered key validators
+    and the first non-None result will be returned.
+
+    Args:
+        apikey (str): the API key to check
+
+    Returns:
+        octoprint.access.users.User: the user found, or None if none was found
+    """
     if apikey is not None:
         if apikey == settings().get(["api", "key"]):
             # master key was used
@@ -273,7 +211,14 @@ def get_user_for_apikey(apikey):
     return None
 
 
-def get_user_for_remote_user_header(request):
+def get_user_for_remote_user_header(
+    request: _flask.Request,
+) -> "Optional[octoprint.access.users.User]":
+    """
+    Tries to find a user based on the configured remote user request header.
+
+    Will only perform any action if the trustRemoteUser setting is enabled.
+    """
     if not settings().getBoolean(["accessControl", "trustRemoteUser"]):
         return None
 
@@ -281,13 +226,13 @@ def get_user_for_remote_user_header(request):
     if header is None:
         return None
 
-    user = octoprint.server.userManager.findUser(userid=header)
+    user = octoprint.server.userManager.find_user(userid=header)
 
     if user is None and settings().getBoolean(["accessControl", "addRemoteUsers"]):
         octoprint.server.userManager.add_user(
             header, settings().generateApiKey(), active=True
         )
-        user = octoprint.server.userManager.findUser(userid=header)
+        user = octoprint.server.userManager.find_user(userid=header)
 
     if user:
         _flask.session["login_mechanism"] = LoginMechanism.REMOTE_USER
@@ -295,24 +240,41 @@ def get_user_for_remote_user_header(request):
     return user
 
 
-def get_user_for_authorization_header(header):
+def get_user_for_authorization_header(
+    request: _flask.Request, header: str = "Authorization"
+) -> "Optional[octoprint.access.users.User]":
+    """
+    Tries to find a user based on the Authorization request header.
+
+    Will only perform any action if the trustBasicAuthentication setting is enabled.
+
+    If configured accordingly, will also check if the password used for the Basic Authentication
+    matches the one stored for the user.
+
+    Args:
+        request: the request object
+        header (str): the header to check for the authorization header, defaults to "Authorization"
+    """
+
+    value = request.headers.get(header)
+
     if not settings().getBoolean(["accessControl", "trustBasicAuthentication"]):
         return None
 
-    if header is None:
+    if value is None:
         return None
 
-    if not header.startswith("Basic "):
-        # we currently only support Basic Authentication
+    if not value.startswith("Basic "):
+        # we only support Basic Authentication here
         return None
 
-    header = header.replace("Basic ", "", 1)
+    value = value.replace("Basic ", "", 1)
     try:
-        header = to_unicode(base64.b64decode(header))
+        value = to_unicode(base64.b64decode(value))
     except TypeError:
         return None
 
-    name, password = header.split(":", 1)
+    name, password = value.split(":", 1)
     if not octoprint.server.userManager.enabled:
         return None
 
@@ -329,7 +291,23 @@ def get_user_for_authorization_header(header):
     return user
 
 
-def get_api_key(request):
+def get_api_key(
+    request: Union[_flask.Request, "tornado.httputil.HTTPServerRequest"],
+) -> Optional[str]:
+    """
+    Extracts the API key from the given request.
+
+    The request may be a Flask or Tornado request object. Attempts will
+    be made to read the API key from the "apikey" request parameter,
+    the "X-Api-Key" header, or the "Authorization" header in "Bearer" mode.
+
+    Args:
+        request: the request object, either a Flask or a Tornado request
+
+    Returns:
+        str: the API key, or None if not found
+    """
+
     # Check Flask GET/POST arguments
     if hasattr(request, "values") and "apikey" in request.values:
         return request.values["apikey"]
@@ -355,11 +333,6 @@ def get_api_key(request):
             return token
 
     return None
-
-
-def get_authorization_header(request):
-    # Tornado and Flask headers
-    return request.headers.get("Authorization")
 
 
 def get_plugin_hash():
@@ -397,17 +370,6 @@ def has_permissions(*permissions):
 
     Returns: True if the user has all permissions, False otherwise
     """
-    logged_in = False
-
-    try:
-        if loginUserFromApiKey():
-            logged_in = True
-    except InvalidApiKeyException:
-        pass  # ignored
-
-    if not logged_in:
-        loginUserFromAuthorizationHeader()
-
     flask.passive_login()
     return all(p.can() for p in permissions)
 

src/octoprint/server/util/flask.py
@@ -585,7 +585,9 @@ class OctoPrintSessionInterface(flask.sessions.SecureCookieSessionInterface):
         return flask.request.endpoint != "static"
 
     def save_session(self, app, session, response):
-        if flask.g.get("login_via_apikey", False):
+        from octoprint.server.util import LoginMechanism
+
+        if session.get("_login_mechanism") == LoginMechanism.APIKEY:
             return
         return super().save_session(app, session, response)
 

src/octoprint/server/util/tornado.py
@@ -1863,22 +1863,14 @@ def access_validation_factory(app, validator, *args):
 
         wsgi_environ = WsgiInputContainer.environ(request)
         with app.request_context(wsgi_environ):
-            session = app.session_interface.open_session(app, flask.request)
-            user_id = session.get("_user_id")
-            user = None
-
-            # Yes, using protected methods is ugly. But these used to be publicly available in former versions
-            # of flask-login, there are no replacements, and seeing them renamed & hidden in a minor version release
-            # without any mention in the changelog means the public API ain't strictly stable either, so we might
-            # as well make our life easier here and just use them...
-            if user_id is not None and app.login_manager._user_callback is not None:
-                user = app.login_manager._user_callback(user_id)
-            app.login_manager._update_request_context_with_user(user)
-
             try:
+                app.session_interface.open_session(app, flask.request)
+                app.login_manager._load_user()
                 validator(flask.request, *args)
             except HTTPException as e:
                 raise tornado.web.HTTPError(e.code) from e
+            except Exception as e:
+                raise tornado.web.HTTPError(500) from e
 
     return f
 

commit 3f45e667bf84d21df459f9fd86e18968799a41a4

fix: api keys and header auth
Misunderstanding how flask_principal and flask_login interact.

Now checking the headers for API keys and such should actually happen again before
permissions are checked.

Tested:
- regular login and UI use
- access to /api/printer via API key
- denied access when using a wrong API key

Regression probably caused by 3a61906 and for some reason not detected since.

Fixes #5098

src/octoprint/server/__init__.py
@@ -34,6 +34,7 @@ from flask_babel import Babel, gettext, ngettext  # noqa: F401
 from flask_login import (  # noqa: F401
     LoginManager,
     current_user,
+    login_user,
     session_protected,
     user_loaded_from_cookie,
     user_logged_out,
@@ -107,8 +108,6 @@ class OctoPrintAnonymousIdentity(AnonymousIdentity):
         for need in user.needs:
             self.provides.add(need)
 
-
-principals = Principal(app, anonymous_identity=OctoPrintAnonymousIdentity)
 
 import octoprint.access.groups as groups  # noqa: E402
 import octoprint.access.permissions as permissions  # noqa: E402
@@ -1704,6 +1703,20 @@ class Server:
         loginManager.request_loader(load_user_from_request)
 
         loginManager.init_app(app, add_context_processor=False)
+
+        global principals
+        principals = Principal(app, anonymous_identity=OctoPrintAnonymousIdentity)
+
+        def current_user_identity_loader():
+            # load the identity from the current flask_login user
+            if (
+                current_user is not None
+                and current_user.is_active
+                and not current_user.is_anonymous
+            ):
+                return Identity(current_user.get_id())
+
+        principals.identity_loader(current_user_identity_loader)
 
     def _setup_blueprints(self):
         # do not remove or the index view won't be found