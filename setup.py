629c30b095de17896a70c0df3ef1d3c848ba7d21
Gina Häußge <gina@octoprint.org>
2025-09-09 10:39:24 +0200

    chore: pin psutil less aggressively again
    piwheels pulled the broken releases, sadly after 1.11.3 was
    already fully tested.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ core_deps = [
     "libpass>=1.8.1,<2 ; python_version>='3.9'",  # replacement for unmaintained passlib
     "passlib==1.7.4 ; python_version<'3.9'",
     "pathvalidate>=3.2.1,<4",  # later versions require Python 3.9
-    "psutil==6.0.0",  # later versions are currently broken on piwheels under bullseye
+    "psutil>=6.1.1,<7",
     "pydantic>=2.10.6,<3 ; python_version>='3.8'",
     "pylru>=1.2.1,<2",
     "pyserial>=3.5,<4",
