OctoPrint/OctoPrint

commit 1ea98f736f99f5b0736c3142bb81b525b586277b

refactor: use datetime for internal file dates, convert to int on api

src/octoprint/schema/api/job.py
@@ -1,0 +1,34 @@
+from typing import Optional
+
+from octoprint.schema import BaseModel
+
+
+class ApiJobFile(BaseModel):
+    name: Optional[str] = None
+    path: Optional[str] = None
+    display: Optional[str] = None
+    origin: Optional[str] = None
+    size: Optional[int] = None
+    date: Optional[int] = None
+
+
+class ApiJobInfo(BaseModel):
+    file: ApiJobFile
+    estimatedPrintTime: Optional[float] = None
+    filament: Optional[dict[str, dict[str, float]]] = None
+    user: Optional[str] = None
+
+
+class ApiProgressInfo(BaseModel):
+    completion: Optional[int] = None
+    filepos: Optional[int] = None
+    printTime: Optional[int] = None
+    printTimeLeft: Optional[int] = None
+    printTimeLeftOrigin: Optional[str] = None
+
+
+class ApiJobResponse(BaseModel):
+    job: ApiJobInfo
+    progress: ApiProgressInfo
+    state: str
+    error: Optional[str] = None

src/octoprint/server/api/job.py
@@ -5,6 +5,7 @@ __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms
 from flask import abort, jsonify, request
 
 from octoprint.access.permissions import Permissions
+from octoprint.schema.api import job as apischema
 from octoprint.server import NO_CONTENT, current_user, printer
 from octoprint.server.api import api
 from octoprint.server.util.flask import get_json_command_from_request, no_firstrun_access
@@ -71,13 +72,38 @@ def controlJob():
 @api.route("/job", methods=["GET"])
 @Permissions.STATUS.require(403)
 def jobState():
-    currentData = printer.get_current_data()
-    response = {
-        "job": currentData["job"],
-        "progress": currentData["progress"],
-        "state": currentData["state"]["text"],
-    }
-    if currentData["state"]["error"]:
-        response["error"] = currentData["state"]["error"]
+    current_data = printer.get_current_data()
 
-    return jsonify(**response)
+    file_data = current_data["job"].get("job", {})
+
+    timestamp = None
+    date = file_data.get("date")
+    if date:
+        timestamp = date.astimezone(None).timestamp()
+
+    file = apischema.ApiJobFile(
+        name=file_data.get("name"),
+        path=file_data.get("path"),
+        display=file_data.get("display"),
+        origin=file_data.get("origin"),
+        size=file_data.get("size"),
+        date=timestamp,
+    )
+
+    job_data = current_data["job"]
+    job = apischema.ApiJobInfo(
+        file=file,
+        estimatedPrintTime=job_data.get("estimatedPrintTime"),
+        filament=job_data.get("filament"),
+        user=job_data.get("user"),
+    )
+
+    progress = apischema.ApiProgressInfo(**current_data["progress"])
+
+    response = apischema.ApiJobResponse(
+        job, progress, state=current_data["state"]["text"]
+    )
+    if current_data["state"]["error"]:
+        response.error = current_data["state"]["error"]
+
+    return jsonify(**response.model_dump(by_alias=True, exclude_none=True))

src/octoprint/printer/job.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Optional
 
 from octoprint.schema import BaseModel
@@ -19,7 +20,7 @@ class PrintJob(BaseModel):
     path: str
     display: str
     size: int = 0
-    date: Optional[int] = None
+    date: Optional[datetime.datetime] = None
     owner: Optional[str] = None
     duration_estimate: Optional[DurationEstimate] = None
     filament_estimate: dict[str, FilamentEstimate] = {}

src/octoprint/filemanager/storage/__init__.py
@@ -3,6 +3,7 @@ __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agp
 __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
 
+import datetime
 from typing import IO, TYPE_CHECKING, Any, Optional
 
 from octoprint.filemanager.util import AbstractFileWrapper
@@ -33,7 +34,7 @@ class StorageCapabilities(BaseModel):
 
 
 class HistoryEntry(BaseModel):
-    timestamp: float
+    timestamp: datetime.datetime
     success: bool
     printerProfile: str
     printTime: Optional[float] = None
@@ -89,7 +90,7 @@ class StorageEntry(BaseModel):
     path: str
     user: Optional[str] = None
 
-    date: Optional[int] = None
+    date: Optional[datetime.datetime] = None
     size: Optional[int] = None
 
     entry_type: str
@@ -113,8 +114,8 @@ class StorageThumbnail(BaseModel):
     printable: str
     sizehint: str
     mime: str = "application/octet-stream"
+    last_modified: Optional[datetime.datetime] = None
     size: int = -1
-    last_modified: int = -1
 
 
 class StorageMeta(BaseModel):

src/octoprint/filemanager/storage/local.py
@@ -4,6 +4,7 @@ __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms
 
 
 import copy
+import datetime
 import logging
 import os
 import shutil
@@ -26,6 +27,7 @@ from octoprint.util import (
     yaml,
 )
 from octoprint.util.files import sanitize_filename
+from octoprint.util.tz import LOCAL_TZ
 
 from . import (
     AnalysisDimensions,
@@ -1385,7 +1387,9 @@ class LocalFileStorage(StorageInterface):
                                     continue
                                 history.append(
                                     HistoryEntry(
-                                        timestamp=h["timestamp"],
+                                        timestamp=datetime.datetime.fromtimestamp(
+                                            h["timestamp"], tz=LOCAL_TZ
+                                        ),
                                         success=h["success"],
                                         printerProfile=h["printerProfile"],
                                         printTime=h.get("printTime"),
@@ -1422,7 +1426,10 @@ class LocalFileStorage(StorageInterface):
 
                     if stat:
                         storage_entry.size = stat.st_size
-                        storage_entry.date = int(stat.st_mtime)
+                        storage_entry.date = datetime.datetime.fromtimestamp(
+                            stat.st_mtime,
+                            tz=LOCAL_TZ,
+                        )
 
                     thumbnails = self._get_thumbnails(os.path.dirname(path_on_disk), name)
                     if thumbnails:

src/octoprint/filemanager/storage/printer.py
@@ -109,7 +109,7 @@ class PrinterFileStorage(StorageInterface):
     def get_lastmodified(self, path=None, recursive=False) -> Optional[int]:
         files = self._get_printer_files(path=path)
 
-        dates = [f.date for f in files if f.date is not None]
+        dates = [f.date.timestamp() for f in files if f.date is not None]
         if len(dates):
             return max(dates)
 
@@ -154,6 +154,8 @@ class PrinterFileStorage(StorageInterface):
     ) -> dict[str, StorageEntry]:
         files = self._get_printer_files(path=path, filter=filter, refresh=force_refresh)
 
+        files = sorted(files, key=lambda x: x.path)
+
         prefix = f"{path}/" if path else ""
         if not recursive:
             if level > 0:
@@ -172,6 +174,9 @@ class PrinterFileStorage(StorageInterface):
 
         result = {}
         for f in files:
+            if f.path == prefix:
+                continue
+
             type_path = get_file_type(f.path)
             if not type_path:
                 if f.path.endswith("/"):
@@ -202,7 +207,7 @@ class PrinterFileStorage(StorageInterface):
                     thumbnails=f.thumbnails,
                 )
 
-            if f.size is not None:
+            if f.size is not None and file_type != "folder":
                 entry.size = f.size
             if f.date is not None:
                 entry.date = f.date
@@ -226,6 +231,21 @@ class PrinterFileStorage(StorageInterface):
 
         if filter is not None:
             result = {k: v for k, v in result.items() if filter(v)}
+
+        def _add_calculated_size(node: StorageFolder) -> int:
+            size = 0
+            for child in node.children.values():
+                size += (
+                    _add_calculated_size(child)
+                    if isinstance(child, StorageFolder)
+                    else child.size
+                )
+            node.size = size
+            return size
+
+        for node in result.values():
+            if isinstance(node, StorageFolder):
+                _add_calculated_size(node)
 
         return result
 

src/octoprint/printer/__init__.py
@@ -26,6 +26,7 @@ __author__ = "Gina Häußge <osd@foosel.net>"
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
+import datetime
 import re
 from typing import IO, TYPE_CHECKING, Optional, Union
 
@@ -574,7 +575,7 @@ class PrinterFile(BaseModel):
     path: str
     display: str
     size: Optional[int] = None
-    date: Optional[int] = None
+    date: Optional[datetime.datetime] = None
     metadata: Optional[MetadataEntry] = None
     thumbnails: list[str] = []
 
@@ -1006,7 +1007,6 @@ class PrinterCallback:
                     origin: <origin of the file, "local" or "printer">,
                     date: <last modification date of the file>
                 estimatedPrintTime: <estimated print time of the file in seconds>
-                lastPrintTime: <last print time of the file in seconds>
                 filament:
                     length: <estimated length of filament needed for this file, in mm>
                     volume: <estimated volume of filament needed for this file, in ccm>

src/octoprint/printer/standard.py
@@ -1799,8 +1799,6 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
                             date=None,
                         ),
                         estimatedPrintTime=None,
-                        averagePrintTime=None,
-                        lastPrintTime=None,
                         filament=None,
                         user=None,
                     )
@@ -1834,7 +1832,7 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
                         display=display_name,
                         origin=job.storage,
                         size=job.size,
-                        date=job.date,
+                        date=int(job.date.astimezone(None).timestamp()),
                     ),
                     estimatedPrintTime=estimatedPrintTime,
                     filament=filament,

commit 26c7930c7f72e66dccacfcebfa3daa3ef03a1051

fix(api): fix error 500 on /api/job endpoint
Fix multiple bugs introduced by https://github.com/OctoPrint/OctoPrint/commit/1ea98f736:

- filament schema was wrong
- file data was read from wrong key ("job" instead of "file")
- date conversion failed because it was already an int
- response schema validation failed because pydantic v2 doesn't support positional args

src/octoprint/schema/api/job.py
@@ -15,7 +15,7 @@ class ApiJobFile(BaseModel):
 class ApiJobInfo(BaseModel):
     file: ApiJobFile
     estimatedPrintTime: Optional[float] = None
-    filament: Optional[dict[str, dict[str, float]]] = None
+    filament: Optional[dict[str, Optional[dict[str, float]]]] = None
     user: Optional[str] = None
 
 

src/octoprint/server/api/job.py
@@ -74,12 +74,7 @@ def controlJob():
 def jobState():
     current_data = printer.get_current_data()
 
-    file_data = current_data["job"].get("job", {})
-
-    timestamp = None
-    date = file_data.get("date")
-    if date:
-        timestamp = date.astimezone(None).timestamp()
+    file_data = current_data["job"].get("file", {})
 
     file = apischema.ApiJobFile(
         name=file_data.get("name"),
@@ -87,7 +82,7 @@ def jobState():
         display=file_data.get("display"),
         origin=file_data.get("origin"),
         size=file_data.get("size"),
-        date=timestamp,
+        date=file_data.get("date"),
     )
 
     job_data = current_data["job"]
@@ -101,7 +96,7 @@ def jobState():
     progress = apischema.ApiProgressInfo(**current_data["progress"])
 
     response = apischema.ApiJobResponse(
-        job, progress, state=current_data["state"]["text"]
+        job=job, progress=progress, state=current_data["state"]["text"]
     )
     if current_data["state"]["error"]:
         response.error = current_data["state"]["error"]