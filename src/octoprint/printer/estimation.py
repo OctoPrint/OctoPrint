OctoPrint/OctoPrint

commit c74e3a0a0b8f98582b979e29658205019cd09c99

don't multiply with -1

src/octoprint/printer/estimation.py
@@ -126,7 +126,7 @@ class PrintTimeEstimator(object):
 					printTimeLeftOrigin = "estimate"
 
 				# combine
-				totalPrintTime = (1.0 - sub_progress) * statisticalTotalPrintTime \
+				totalPrintTime = - sub_progress * statisticalTotalPrintTime \
 				                 + sub_progress * estimatedTotalPrintTime
 
 		printTimeLeft = None
@@ -204,7 +204,7 @@ class TimeEstimationHelper(object):
 		if old_average_total:
 			self._distances.append(abs(self.average_total - old_average_total))
 
-		if -1.0 * self._threshold < self.average_distance < self._threshold:
+		if -self._threshold < self.average_distance < self._threshold:
 			self._stable_counter += 1
 		else:
 			self._stable_counter = -1

commit b4699c0bc0237e6acde88377a969645b2e48f603

fix(printer): fix estimation percentage based weighing
This bug was introduced by commit https://github.com/OctoPrint/OctoPrint/commit/c74e3a0a0

src/octoprint/printer/estimation.py
@@ -145,9 +145,8 @@ class PrintTimeEstimator:
 
                 # combine
                 totalPrintTime = (
-                    -sub_progress * statisticalTotalPrintTime
-                    + sub_progress * estimatedTotalPrintTime
-                )
+                    1.0 - sub_progress
+                ) * statisticalTotalPrintTime + sub_progress * estimatedTotalPrintTime
 
         printTimeLeft = None
         if totalPrintTime is not None: