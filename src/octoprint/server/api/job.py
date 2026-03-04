OctoPrint/OctoPrint

commit dcc5a48fb1b7fe89db5f2b5c34014d434004b323

docs: update API docs for 1.12.0 changes

src/octoprint/server/api/job.py
@@ -77,9 +77,24 @@ def controlJob():
 @api_versioned
 @Permissions.STATUS.require(403)
 def jobState():
-    response = _get_api_job_response().model_dump(by_alias=True)
-    response["job"]["lastPrintTime"] = None  # backwards compatibility
-    return jsonify(**response)
+    response = _get_api_job_response()
+
+    job_info = response.job
+    job_info_pre_1_12 = apischema.ApiJobInfo_pre_1_12(
+        file=job_info.file,
+        estimatedPrintTime=job_info.estimatedPrintTime,
+        lastPrintTime=None,
+        filament=job_info.filament,
+        user=job_info.user,
+    )
+
+    response_pre_1_12 = apischema.ApiJobResponse_pre_1_12(
+        job=job_info_pre_1_12,
+        progress=response.progress,
+        state=response.state,
+    )
+
+    return jsonify(**response_pre_1_12.model_dump(by_alias=True))
 
 
 @jobState.version(">=1.12.0")
@@ -88,7 +103,7 @@ def jobState_post_1_12():
     return jsonify(**_get_api_job_response().model_dump(by_alias=True, exclude_none=True))
 
 
-def _get_api_job_response():
+def _get_api_job_response() -> apischema.ApiJobResponse:
     current_data = printer.get_current_data()
 
     file_data = current_data["job"].get("file", {})

src/octoprint/schema/api/job.py
@@ -1,34 +1,82 @@
 from typing import Optional
 
 from octoprint.schema import BaseModel
+from octoprint.vendor.with_attrs_docs import with_attrs_docs
 
 
+@with_attrs_docs
 class ApiJobFile(BaseModel):
     name: Optional[str] = None
+    """Internal name of the file being printed"""
     path: Optional[str] = None
+    """Path of the file being printed"""
     display: Optional[str] = None
+    """Display name of the file being printed"""
     origin: Optional[str] = None
+    """Storage of the file being printed"""
     size: Optional[int] = None
+    """Size of the file being printed in bytes"""
     date: Optional[int] = None
+    """Last modification date of the file being printed as timestamp"""
 
 
+@with_attrs_docs
 class ApiJobInfo(BaseModel):
     file: ApiJobFile
+    """File being printed"""
     estimatedPrintTime: Optional[float] = None
+    """Estimated print time in seconds, if known"""
     filament: Optional[dict[str, Optional[dict[str, float]]]] = None
+    """Filament usage information as mapping from printer profile to mappings from tool to used length, if known"""
     user: Optional[str] = None
+    """The use who started the job, if known"""
 
 
+@with_attrs_docs
+class ApiJobInfo_pre_1_12(ApiJobInfo):
+    lastPrintTime: Optional[float] = None
+    """The last print time in seconds"""
+
+
+@with_attrs_docs
 class ApiProgressInfo(BaseModel):
     completion: Optional[int] = None
+    """Completion in percentage, if known"""
     filepos: Optional[int] = None
+    """Current file position, if known"""
     printTime: Optional[int] = None
+    """Print time so far"""
     printTimeLeft: Optional[int] = None
+    """Estimated print time left"""
     printTimeLeftOrigin: Optional[str] = None
+    """
+    Origin of estimate.
+
+    E.g.:
+
+    * ``linear``: based on a linear approximation of the progress in file in bytes vs time
+    * ``analysis``: based on an analysis of the file
+    * ``estimate``: calculated estimate after stabilization of linear estimation
+    * ``average``: based on the average total from past prints of the same model against the same printer profile
+    * ``mixed-analysis``: mixture of ``estimate`` and ``analysis``
+    * ``mixed-average``: mixture of ``estimate`` and ``average``
+    * ``printer``: estimate by printer
+    """
 
 
+@with_attrs_docs
 class ApiJobResponse(BaseModel):
     job: ApiJobInfo
+    """Information about the current job"""
     progress: ApiProgressInfo
+    """Information about the current job's progress"""
     state: str
+    """Current state"""
     error: Optional[str] = None
+    """Error, if any"""
+
+
+@with_attrs_docs
+class ApiJobResponse_pre_1_12(ApiJobResponse):
+    job: ApiJobInfo_pre_1_12
+    """Information about the current job"""

src/octoprint/filemanager/storage/__init__.py
@@ -9,11 +9,13 @@ from typing import IO, TYPE_CHECKING, Any, Optional
 from octoprint.filemanager.util import AbstractFileWrapper
 from octoprint.schema import BaseModel
 from octoprint.util import deprecated
+from octoprint.vendor.with_attrs_docs import with_attrs_docs
 
 if TYPE_CHECKING:
     from octoprint.printer.job import PrintJob  # noqa: F401
 
 
+@with_attrs_docs
 class StorageCapabilities(BaseModel):
     write_file: bool = False
     """Storage supports writing files"""

commit 384c58bf43087425f340221dadc9d81e1a91bceb

fix(api): preserve error in job state pre-1.12 response
This bug was introduced by https://github.com/OctoPrint/OctoPrint/commit/dcc5a48fb1b7fe89db5f2b5c34014d434004b323

src/octoprint/server/api/job.py
@@ -92,6 +92,7 @@ def jobState():
         job=job_info_pre_1_12,
         progress=response.progress,
         state=response.state,
+        error=response.error,
     )
 
     return jsonify(**response_pre_1_12.model_dump(by_alias=True))