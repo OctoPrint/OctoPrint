OctoPrint/OctoPrint

commit 82366d05c9bf2a43c920cac765d9b06e95db6aa1

refactor: rename anything "sd" on APIs & UI to "(printer) storage"
Of course, keep backwards compatibility, either through fallbacks with
deprecation warnings, or API versioning.

Document everything accordingly.

Should reduce confusion (see also #5244) and align things better
with modern printers that don't have an SD card anymore.

src/octoprint/static/js/app/client/printer.js
@@ -10,7 +10,7 @@
     var toolUrl = url + "/tool";
     var bedUrl = url + "/bed";
     var chamberUrl = url + "/chamber";
-    var sdUrl = url + "/sd";
+    var storageUrl = url + "/storage";
     var errorUrl = url + "/error";
 
     var OctoPrintPrinterClient = function (base) {
@@ -45,8 +45,18 @@
         return this.base.issueCommand(chamberUrl, command, payload, opts);
     };
 
+    OctoPrintPrinterClient.prototype.issueStorageCommand = function (
+        command,
+        payload,
+        opts
+    ) {
+        return this.base.issueCommand(storageUrl, command, payload, opts);
+    };
     OctoPrintPrinterClient.prototype.issueSdCommand = function (command, payload, opts) {
-        return this.base.issueCommand(sdUrl, command, payload, opts);
+        log.warn(
+            "OctoPrintClient.printer.issueSdCommand has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.issueStorageCommand instead"
+        );
+        return this.base.issueStorageCommand(command, payload, opts);
     };
 
     OctoPrintPrinterClient.prototype.getFullState = function (flags, opts) {
@@ -67,7 +77,10 @@
             }
 
             if (exclude) {
-                getUrl += "exclude=" + exclude.join(",") + "&";
+                getUrl +=
+                    "exclude=" +
+                    exclude.map((val) => (val === "sd" ? "storage" : val)).join(",") +
+                    "&";
             }
         }
 
@@ -125,8 +138,14 @@
         return this.base.get(getUrl, opts);
     };
 
+    OctoPrintPrinterClient.prototype.getStorageState = function (opts) {
+        return this.base.get(storageUrl, opts);
+    };
     OctoPrintPrinterClient.prototype.getSdState = function (opts) {
-        return this.base.get(sdUrl, opts);
+        log.warn(
+            "OctoPrintClient.printer.getSdState has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.getStorageState instead"
+        );
+        return OctoPrintClient.prototype.getStorageState(opts);
     };
 
     OctoPrintPrinterClient.prototype.getErrorInfo = function (opts) {
@@ -269,16 +288,31 @@
         return this.issueChamberCommand("offset", payload, opts);
     };
 
+    OctoPrintPrinterClient.prototype.initStorage = function (opts) {
+        return this.issueStorageCommand("init", {}, opts);
+    };
     OctoPrintPrinterClient.prototype.initSd = function (opts) {
-        return this.issueSdCommand("init", {}, opts);
+        log.warn(
+            "OctoPrintClient.printer.getSdState has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.getStorageState instead"
+        );
+        return this.base.initStorage(opts);
+    };
+
+    OctoPrintPrinterClient.prototype.releaseStorage = function (opts) {
+        return this.issueStorageCommand("release", {}, opts);
+    };
+    OctoPrintPrinterClient.prototype.releaseSd = function (opts) {
+        log.warn(
+            "OctoPrintClient.printer.releaseSd has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.releaseStorage instead"
+        );
+        return this.base.releaseStorage(opts);
     };
 
     OctoPrintPrinterClient.prototype.refreshSd = function (opts) {
-        return this.issueSdCommand("refresh", {}, opts);
-    };
-
-    OctoPrintPrinterClient.prototype.releaseSd = function (opts) {
-        return this.issueSdCommand("release", {}, opts);
+        log.warn(
+            "OctoPrintClient.printer.refreshSd has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.files.listForLocation instead"
+        );
+        return this.issueStorageCommand("refresh", {}, opts);
     };
 
     OctoPrintClient.registerComponent("printer", OctoPrintPrinterClient);

src/octoprint/plugins/serial_connector/connector.py
@@ -556,11 +556,12 @@ class ConnectedSerialPrinter(ConnectedPrinter, PrinterFilesMixin):
             raise PrinterFilesUnavailableError(message)
 
         try:
+            tags = kwargs.get("tags", set()) | {"trigger:printer.add_sd_file"}
             return self._comm.startFileTransfer(
                 source,
                 target,
                 special=not valid_file_type(target, "gcode"),
-                tags=kwargs.get("tags", set()),
+                tags=tags,
             )
         except Exception as exc:
             self._logger.exception("Error while starting file transfer")

src/octoprint/printer/__init__.py
@@ -99,7 +99,7 @@ class ConnectedPrinterCapabilities(BaseModel):
 
 class CommonPrinterMixin:
     """
-    The :class:`PrinterInterface` represents the developer interface to the :class:`~octoprint.printer.standard.Printer`
+    The :class:`CommonPrinterMixin` represents the developer interface to the :class:`~octoprint.printer.standard.Printer`
     instance.
     """
 
@@ -465,6 +465,15 @@ class CommonPrinterMixin:
         Returns:
             (list) The temperature history.
         """
+
+    def is_storage_mounted(self, *args, **kwargs):
+        """Returns whether the printer has its internal storage mounted or not (if supported)"""
+
+    def mount_storage(self, *args, **kwargs):
+        """Tells the printer to mount its internal storage (if supported)"""
+
+    def unmount_storage(self, *args, **kwargs):
+        """Tells the printer to unmount its internal storage (if supported)"""
 
     def get_current_connection(self, *args, **kwargs):
         """
@@ -842,7 +851,7 @@ class PrinterMixin(CommonPrinterMixin):
 
     @deprecated(
         message="select_file has been deprecated and will be removed in a future version. Please use set_job instead.",
-        includedoc="Replaced by :func:`PrinterInterface.set_job`",
+        includedoc="Replaced by :func:`PrinterMixin.set_job`",
         since="1.12.0",
     )
     def select_file(
@@ -882,7 +891,7 @@ class PrinterMixin(CommonPrinterMixin):
 
     @deprecated(
         message="unselect_file has been deprecated and will be removed in a future version. Please use set_job instead.",
-        includedoc="Replaced by :func:`PrinterInterface.set_job`",
+        includedoc="Replaced by :func:`PrinterMixin.set_job`",
         since="1.12.0",
     )
     def unselect_file(self, *args, **kwargs):
@@ -893,7 +902,7 @@ class PrinterMixin(CommonPrinterMixin):
 
     @deprecated(
         message="fake_ack has been renamed to repair_communication. This compatibility layer will be removed in a future version. Please use repair_communication instead.",
-        includedoc="Replaced by :func:`PrinterInterface.repair_communication`",
+        includedoc="Replaced by :func:`PrinterMixin.repair_communication`",
         since="1.12.0",
     )
     def fake_ack(self, *args, **kwargs):
@@ -914,6 +923,32 @@ class PrinterMixin(CommonPrinterMixin):
         Returns:
             object: The communication layer's transport object
         """
+
+    ### Printer storage stuff
+
+    @deprecated(
+        message="is_sd_ready has been deprecated and will be removed in a future version. Please use is_storage_mounted instead.",
+        includedoc="Replaced by :func:`PrinterMixin.is_storage_mounted`",
+        since="1.12.0",
+    )
+    def is_sd_ready(self, *args, **kwargs):
+        return self.is_storage_mounted()
+
+    @deprecated(
+        message="init_sd_card has been deprecated and will be removed in a future version. Please use mount_storage instead.",
+        includedoc="Replaced by :func:`PrinterMixin.mount_storage`",
+        since="1.12.0",
+    )
+    def init_sd_card(self, *args, **kwargs):
+        return self.mount_storage(*args, **kwargs)
+
+    @deprecated(
+        message="release_sd_card has been deprecated and will be removed in a future version. Please use unmount_storage instead.",
+        includedoc="Replaced by :func:`PrinterMixin.unmount_storage`",
+        since="1.12.0",
+    )
+    def release_sd_card(self, *args, **kwargs):
+        return self.unmount_storage(*args, **kwargs)
 
 
 PrinterInterface = PrinterMixin

src/octoprint/printer/standard.py
@@ -1030,9 +1030,31 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
         for line in lines:
             self._add_log(line)
 
-    # ~~ sd file handling
+    # ~~ printer storage
 
-    def is_sd_ready(self, *args, **kwargs):
+    def mount_storage(self, *args, **kwargs):
+        if (
+            not self._connection
+            or not isinstance(self._connection, PrinterFilesMixin)
+            or self.is_storage_mounted()
+        ):
+            return
+
+        tags = kwargs.get("tags", set()) | {"trigger:printer.mount_storage"}
+
+        self._connection.mount_printer_files(tags=tags)
+
+    def unmount_storage(self, *args, **kwargs):
+        if not self.is_storage_mounted():
+            return
+
+        tags = kwargs.get("tags", set()) | {"trigger:printer.unmount_storage"}
+
+        cast(PrinterFilesMixin, self._connection).unmount_printer_files(tags=tags)
+
+    # ~~ deprecated printer storage stuff, now to be done through dedicated PrinterStorage
+
+    def is_storage_mounted(self, *args, **kwargs):
         if (
             not settings().getBoolean(["feature", "sdSupport"])
             or self._connection is None
@@ -1042,8 +1064,13 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
 
         return self._connection.printer_files_mounted
 
+    @util.deprecated(
+        message="get_sd_files has been deprecated and will be removed in a future version. Please use the PrinterStorage instead.",
+        includedoc="Functionality moved to :class:`~octoprint.filemanager.storage.printer.PrinterStorage`",
+        since="1.12.0",
+    )
     def get_sd_files(self, *args, **kwargs):
-        if not self.is_sd_ready():
+        if not self.is_storage_mounted():
             return []
 
         refresh = kwargs.get("refresh", False)
@@ -1055,10 +1082,15 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
             )
         ]
 
+    @util.deprecated(
+        message="add_sd_file has been deprecated and will be removed in a future version. Please use the PrinterStorage instead.",
+        includedoc="Functionality moved to :class:`~octoprint.filemanager.storage.printer.PrinterStorage`",
+        since="1.12.0",
+    )
     def add_sd_file(
         self, filename, path, on_success=None, on_failure=None, *args, **kwargs
     ):
-        if not self.is_sd_ready() or not self._connection.is_ready():
+        if not self.is_storage_mounted() or not self._connection.is_ready():
             self._logger.error("No connection to printer or printer is busy")
             return
 
@@ -1123,50 +1155,34 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
 
         else:
             # no plugin feels responsible, use the default implementation
-            tags = kwargs.get("tags", set()) | {"trigger:printer.add_sd_file"}
             local = self._file_manager.path_on_disk(FileDestinations.LOCAL, filename)
-            remote = connection.upload_printer_file(local, path, tags=tags)
+            remote = connection.upload_printer_file(local, path)
             return remote
 
+    @util.deprecated(
+        message="delete_sd_file has been deprecated and will be removed in a future version. Please use the PrinterStorage instead.",
+        includedoc="Functionality moved to :class:`~octoprint.filemanager.storage.printer.PrinterStorage`",
+        since="1.12.0",
+    )
     def delete_sd_file(self, filename, *args, **kwargs):
-        if not self.is_sd_ready():
+        if not self.is_storage_mounted():
             return
 
-        tags = kwargs.get("tags", set()) | {"trigger:printer.delete_sd_file"}
+        cast(PrinterFilesMixin, self._connection).delete_printer_file("/" + filename)
 
-        cast(PrinterFilesMixin, self._connection).delete_printer_file(
-            "/" + filename, tags=tags
-        )
-
-    def init_sd_card(self, *args, **kwargs):
-        if (
-            not self._connection
-            or not isinstance(self._connection, PrinterFilesMixin)
-            or self.is_sd_ready()
-        ):
-            return
-
-        tags = kwargs.get("tags", set()) | {"trigger:printer.init_sd_card"}
-
-        self._connection.mount_printer_files(tags=tags)
-
-    def release_sd_card(self, *args, **kwargs):
-        if not self.is_sd_ready():
-            return
-
-        tags = kwargs.get("tags", set()) | {"trigger:printer.release_sd_card"}
-
-        cast(PrinterFilesMixin, self._connection).unmount_printer_files(tags=tags)
-
+    @util.deprecated(
+        message="refresh_sd_files has been deprecated and will be removed in a future version. Please use the PrinterStorage instead.",
+        includedoc="Functionality moved to :class:`~octoprint.filemanager.storage.printer.PrinterStorage`",
+        since="1.12.0",
+    )
     def refresh_sd_files(self, blocking=False, *args, **kwargs):
-        if not self.is_sd_ready():
+        if not self.is_storage_mounted():
             return
 
-        tags = kwargs.get("tags", set()) | {"trigger:printer.refresh_sd_files"}
         timeout = kwargs.get("timeout", 10)
 
         cast(PrinterFilesMixin, self._connection).refresh_printer_files(
-            blocking=blocking, timeout=timeout, tags=tags
+            blocking=blocking, timeout=timeout
         )
 
     # ~~ ConnectedPrinterListenerMixin
@@ -1893,7 +1909,7 @@ class Printer(PrinterMixin, ConnectedPrinterListenerMixin):
             error=self.is_error(),
             paused=self.is_paused(),
             ready=self.is_ready(),
-            sdReady=self.is_sd_ready(),
+            sdReady=self.is_storage_mounted(),
         )
 
     def _payload_for_print_job_event(

src/octoprint/server/api/printer.py
@@ -8,7 +8,7 @@ from flask import Response, abort, jsonify, request
 
 from octoprint.access.permissions import Permissions
 from octoprint.printer import UnknownScript
-from octoprint.server import NO_CONTENT, printer, printerProfileManager
+from octoprint.server import NO_CONTENT, fileManager, printer, printerProfileManager
 from octoprint.server.api import api
 from octoprint.server.util.flask import (
     api_version_matches,
@@ -33,7 +33,7 @@ def printerState():
         if len(excludeStr.strip()) > 0:
             excludes = list(
                 filter(
-                    lambda x: x in ["temperature", "sd", "state"],
+                    lambda x: x in ["temperature", "storage", "sd", "state"],
                     (x.strip() for x in excludeStr.split(",")),
                 )
             )
@@ -54,9 +54,14 @@ def printerState():
 
         result.update({"temperature": _get_temperature_data(processor)})
 
-    # add sd information
-    if "sd" not in excludes and settings().getBoolean(["feature", "sdSupport"]):
-        result.update({"sd": {"ready": printer.is_sd_ready()}})
+    # add storage information
+    if api_version_matches(">=1.12.0"):
+        storage_key = "storage"
+    else:
+        storage_key = "sd"
+
+    if storage_key not in excludes and settings().getBoolean(["feature", "sdSupport"]):
+        result.update({storage_key: {"ready": printer.is_storage_mounted()}})
 
     # add state information
     if "state" not in excludes:
@@ -351,15 +356,16 @@ def printerPrintheadCommand():
     return NO_CONTENT
 
 
-##~~ SD Card
+##~~ Printer side storage
 
 
-@api.route("/printer/sd", methods=["POST"])
+@api.route("/printer/storage", methods=["POST"])
+@api.route("/printer/sd", methods=["POST"])  # backwards compatibility
 @no_firstrun_access
 @Permissions.CONTROL.require(403)
-def printerSdCommand():
+def printerStorageCommand():
     if not settings().getBoolean(["feature", "sdSupport"]):
-        abort(404, description="SD support is disabled")
+        abort(404, description="Printer storage support is disabled")
 
     if not printer.is_operational() or printer.is_printing() or printer.is_paused():
         abort(409, description="Printer is not operational or currently busy")
@@ -369,26 +375,27 @@ def printerSdCommand():
     if response is not None:
         return response
 
-    tags = {"source:api", "api:printer.sd"}
+    tags = {"source:api", "api:printer.sd", "api:printer.storage"}
 
     if command == "init":
-        printer.init_sd_card(tags=tags)
+        printer.mount_storage(tags=tags)
     elif command == "refresh":
-        printer.refresh_sd_files(tags=tags)
+        fileManager.list_storage_entries(["printer"], force_refresh=True)
     elif command == "release":
-        printer.release_sd_card(tags=tags)
+        printer.unmount_storage(tags=tags)
 
     return NO_CONTENT
 
 
-@api.route("/printer/sd", methods=["GET"])
+@api.route("/printer/storage", methods=["GET"])
+@api.route("/printer/sd", methods=["GET"])  # backwards compatibility
 @no_firstrun_access
 @Permissions.STATUS.require(403)
-def printerSdState():
+def printerStorageState():
     if not settings().getBoolean(["feature", "sdSupport"]):
-        abort(404, description="SD support is disabled")
+        abort(404, description="Printer storage support is disabled")
 
-    return jsonify(ready=printer.is_sd_ready())
+    return jsonify(ready=printer.is_storage_mounted())
 
 
 ##~~ Commands

src/octoprint/static/js/app/client/files.js
@@ -75,9 +75,23 @@
         recursively,
         opts
     ) {
+        let force;
+        if (typeof opts === "boolean") {
+            force = opts;
+            opts = {};
+        } else {
+            force = false;
+        }
+
         recursively = recursively || false;
+        force = force || false;
+
         return this.base
-            .getWithQuery(resourceForLocation(location), {recursive: recursively}, opts)
+            .getWithQuery(
+                resourceForLocation(location),
+                {recursive: recursively, force: force},
+                opts
+            )
             .done(preProcessList);
     };
 

src/octoprint/static/js/app/viewmodels/files.js
@@ -1020,19 +1020,37 @@ $(function () {
             });
         };
 
+        self.initPrinterStorage = function () {
+            if (!self.loginState.hasPermission(self.access.permissions.CONTROL)) return;
+            OctoPrint.printer.initStorage();
+        };
         self.initSdCard = function () {
-            if (!self.loginState.hasPermission(self.access.permissions.CONTROL)) return;
-            OctoPrint.printer.initSd();
+            log.warn(
+                "initSdCard has been deprecated as of OctoPrint 1.12.0, use initPrinterStorage instead"
+            );
+            self.initPrinterStorage();
         };
 
+        self.releasePrinterStorage = function () {
+            if (!self.loginState.hasPermission(self.access.permissions.CONTROL)) return;
+            OctoPrint.printer.releaseStorage();
+        };
         self.releaseSdCard = function () {
-            if (!self.loginState.hasPermission(self.access.permissions.CONTROL)) return;
-            OctoPrint.printer.releaseSd();
+            log.warn(
+                "releaseSdCard has been deprecated as of OctoPrint 1.12.0, use releasePrinterStorage instead"
+            );
+            self.releasePrinterStorage();
         };
 
-        self.refreshSdFiles = function () {
+        self.refreshPrinterStorage = function () {
             if (!self.loginState.hasPermission(self.access.permissions.CONTROL)) return;
-            OctoPrint.printer.refreshSd();
+            OctoPrint.files.listForLocation("printer", true, true);
+        };
+        self.refreshSdFiles = function () {
+            log.warn(
+                "refreshSdFiles has been deprecated as of OctoPrint 1.12.0, use the printer storage directly instead"
+            );
+            self.refreshPrinterStorage();
         };
 
         self.moveFileOrFolder = function (source, destination, storage) {

src/octoprint/templates/sidebar/files/files_header.jinja2
@@ -89,19 +89,18 @@
         <a class="dropdown-toggle"
            data-toggle="dropdown"
            href="#"
-           title="{{ _('SD Card operations') |edq }}"
-           aria-label="{{ _('SD Card operations') |edq }}">
+           title="{{ _('Printer Storage operations') |edq }}"
+           aria-label="{{ _('Printer Storage operations') |edq }}">
             <i class="fas fa-sd-card"></i>
         </a>
         <ul class="dropdown-menu">
             <li data-bind="visible: !isSdReady()">
-                <a href="#" data-bind="click: function() { $root.initSdCard(); }"><i class="fas fa-flag"></i> {{ _("Initialize SD card") }}</a>
+                <a href="#"
+                   data-bind="click: function() { $root.initPrinterStorage(); }"><i class="fas fa-flag"></i> {{ _("Initialize printer storage") }}</a>
             </li>
             <li data-bind="visible: isSdReady()">
-                <a href="#" data-bind="click: function() { $root.refreshSdFiles(); }"><i class="fas fa-sync"></i> {{ _("Refresh SD files") }}</a>
-            </li>
-            <li data-bind="visible: isSdReady()">
-                <a href="#" data-bind="click: function() { $root.releaseSdCard(); }"><i class="fas fa-eject"></i> {{ _("Release SD card") }}</a>
+                <a href="#"
+                   data-bind="click: function() { $root.releasePrinterStorage(); }"><i class="fas fa-eject"></i> {{ _("Release printer storage") }}</a>
             </li>
         </ul>
     </div>

commit 17ad972964c8b00b34c9cf4362465a1308f26f35

fix(jsclient): broken deprecated sd commands
The deprecated backward-compat jsclient wrappers for sd commands (`issueSdCommand`, `getSdState`, `initSd`, releaseSd) were broken by 82366d05c9: they called methods on `this.base` (the base OctoPrint client) instead of `this` (the printer client), causing a `TypeError` at runtime. Also fixes a copy-paste typo in the `initSd` deprecation warning message.

src/octoprint/static/js/app/client/printer.js
@@ -56,7 +56,7 @@
         log.warn(
             "OctoPrintClient.printer.issueSdCommand has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.issueStorageCommand instead"
         );
-        return this.base.issueStorageCommand(command, payload, opts);
+        return this.issueStorageCommand(command, payload, opts);
     };
 
     OctoPrintPrinterClient.prototype.getFullState = function (flags, opts) {
@@ -145,7 +145,7 @@
         log.warn(
             "OctoPrintClient.printer.getSdState has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.getStorageState instead"
         );
-        return OctoPrintClient.prototype.getStorageState(opts);
+        return this.getStorageState(opts);
     };
 
     OctoPrintPrinterClient.prototype.getErrorInfo = function (opts) {
@@ -293,9 +293,9 @@
     };
     OctoPrintPrinterClient.prototype.initSd = function (opts) {
         log.warn(
-            "OctoPrintClient.printer.getSdState has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.getStorageState instead"
+            "OctoPrintClient.printer.initSd has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.initStorage instead"
         );
-        return this.base.initStorage(opts);
+        return this.initStorage(opts);
     };
 
     OctoPrintPrinterClient.prototype.releaseStorage = function (opts) {
@@ -305,7 +305,7 @@
         log.warn(
             "OctoPrintClient.printer.releaseSd has been deprecated as of OctoPrint 1.12.0, use OctoPrintClient.printer.releaseStorage instead"
         );
-        return this.base.releaseStorage(opts);
+        return this.releaseStorage(opts);
     };
 
     OctoPrintPrinterClient.prototype.refreshSd = function (opts) {