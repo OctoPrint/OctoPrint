OctoPrint/OctoPrint

commit d9f0e005262ab02404418efdd8a133b3422677a9

wip: printer.job -> printer.active_job

src/octoprint/server/api/files.py
@@ -655,7 +655,7 @@ def uploadGcodeFile(target):
             )
 
         reselect = (
-            str(printer.job)
+            str(printer.active_job)
             == f"{FileDestinations.SDCARD if sd else FileDestinations.LOCAL}:{futureFullPathInStorage}"
         )
 

commit 5cb8b3b38ba1e6531b978b11dabfc833ea196a80

fix(api): fix file reselection after upload
When overwriting the currently selected file, it was not reselected because it was compared against active_job (which is None when not printing) instead of current_job.

This bug was introduced by d9f0e0052.

src/octoprint/server/api/files.py
@@ -828,7 +828,7 @@ def uploadGcodeFile(target):
                     description="File already exists, cannot overwrite due to a lack of permissions",
                 )
 
-            reselect = str(printer.active_job) == f"{target}:{futureFullPathInStorage}"
+            reselect = str(printer.current_job) == f"{target}:{futureFullPathInStorage}"
 
             upload_done = False
 