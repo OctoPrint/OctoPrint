a1fa8b8f0e8193d7a85963be0da4030143274b4f
Gina Häußge <gina@octoprint.org>
2026-03-18 16:40:45 +0100

    test: add unit test
    And: migrate existing pretty static unit tests to pure pytest

--- tests/access/users/test_usermanager.py
+++ tests/access/users/test_usermanager.py
@@ -5,33 +5,44 @@ Unit tests for octoprint.access.users.UserManager
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = "Copyright (C) 2017 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
-import unittest
-
-import ddt
+import pytest
 
 import octoprint.access.users
 
 
-@ddt.ddt
-class UserManagerTest(unittest.TestCase):
-    def test_createPasswordHash_nonascii(self):
-        """Test for issue #1891"""
+def test_createPasswordHash_nonascii():
+    """Test for issue #1891"""
 
-        password = "password with ümläutß"
+    password = "password with ümläutß"
 
-        # should not throw an exception
-        octoprint.access.users.UserManager.create_password_hash(password)
+    # should not throw an exception
+    octoprint.access.users.UserManager.create_password_hash(password)
 
-    def test_createPasswordHash_is_valid(self):
-        password = "test1234"
-        password_hash = octoprint.access.users.UserManager.create_password_hash(password)
-        user = octoprint.access.users.User(
-            "username",
-            password_hash,
-            True,
-            permissions=[],
-            apikey="apikey",
-            settings={"key": "value"},
-        )
 
-        self.assertTrue(user.check_password(password))
+def test_createPasswordHash_is_valid():
+    password = "test1234"
+    password_hash = octoprint.access.users.UserManager.create_password_hash(password)
+    user = octoprint.access.users.User(
+        "username",
+        password_hash,
+        True,
+        permissions=[],
+        apikey="apikey",
+        settings={"key": "value"},
+    )
+
+    assert user.check_password(password)
+
+
+@pytest.mark.parametrize("password", [octoprint.access.users.NOLOGIN_PWHASH, "", None])
+def test_nologin_user_check_password_always_false(password):
+    user = octoprint.access.users.User(
+        "username",
+        octoprint.access.users.NOLOGIN_PWHASH,
+        True,
+        permissions=[],
+        apikey="apikey",
+        settings={"key": "value"},
+    )
+
+    assert not user.check_password(password)
