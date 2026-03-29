932b7efafe1c8bb4b07db1f8db6239e8075fb103
Jacopo Tediosi <jacopotediosi@gmail.com>
2026-03-29 12:09:37 +0200

    fix(printer): convert booleans using valid_boolean_trues

--- src/octoprint/printer/profile.py
+++ src/octoprint/printer/profile.py
@@ -151,7 +151,7 @@ import copy
 import logging
 import os
 
-from octoprint.settings import settings
+from octoprint.settings import settings, valid_boolean_trues
 from octoprint.util import (
     dict_contains_keys,
     dict_merge,
@@ -702,10 +702,13 @@ class PrinterProfileManager:
             ("axes", "x", "inverted"),
             ("axes", "y", "inverted"),
             ("axes", "z", "inverted"),
+            ("axes", "e", "inverted"),
             ("extruder", "sharedNozzle"),
+            ("heatedBed",),
+            ("heatedChamber",),
         ):
             try:
-                convert_value(profile, path, bool)
+                convert_value(profile, path, lambda v: v in valid_boolean_trues)
             except Exception as e:
                 self._logger.warning(
                     "Profile has invalid value for path {path!r}: {msg}".format(
